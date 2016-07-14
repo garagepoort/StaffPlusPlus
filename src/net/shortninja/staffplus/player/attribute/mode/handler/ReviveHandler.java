dcc22f985e33a742c61528d9a0f9ade7d2da70f4
Gavin Lutz <Shortninja66@gmail.com>
2016-07-14 03:26:51 -0500

    Clear inventory on revive

--- src/net/shortninja/staffplus/player/attribute/mode/handler/ReviveHandler.java
+++ src/net/shortninja/staffplus/player/attribute/mode/handler/ReviveHandler.java
@@ -8,6 +8,7 @@ import net.shortninja.staffplus.StaffPlus;
 import net.shortninja.staffplus.data.config.Messages;
 import net.shortninja.staffplus.player.attribute.mode.ModeDataVault;
 import net.shortninja.staffplus.util.Message;
+import net.shortninja.staffplus.util.lib.JavaUtils;
 
 import org.bukkit.entity.Player;
 
@@ -35,9 +36,9 @@ public class ReviveHandler
 		UUID uuid = player.getUniqueId();
 		ModeDataVault modeDataVault = savedInventories.get(uuid);
 		
+		JavaUtils.clearInventory(player);
 		player.getInventory().setContents(modeDataVault.getItems());
 		player.getInventory().setArmorContents(modeDataVault.getArmor());
 		message.send(player, messages.revivedUser, messages.prefixGeneral);
-		savedInventories.remove(uuid);
 	}
 }
