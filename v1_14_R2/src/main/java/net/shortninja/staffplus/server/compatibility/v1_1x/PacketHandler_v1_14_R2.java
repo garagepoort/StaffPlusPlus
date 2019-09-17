61a86abb8fdb3b7ab960b29cbe0da669bdb95e58
Qballl <qvanbooven513@gmail.com>
2019-09-17 11:32:29 -0500

    Fixed some merge issues

---
+++ v1_14_R2/src/main/java/net/shortninja/staffplus/server/compatibility/v1_1x/PacketHandler_v1_14_R2.java
@@ -0,0 +1,31 @@
+package net.shortninja.staffplus.server.compatibility.v1_1x;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import net.minecraft.server.v1_14_R1.PacketPlayOutNamedSoundEffect;
+import net.shortninja.staffplus.IStaffPlus;
+import net.shortninja.staffplus.server.compatibility.AbstractPacketHandler;
+import org.bukkit.entity.Player;
+
+public final class PacketHandler_v1_14_R2 extends AbstractPacketHandler {
+
+    public PacketHandler_v1_14_R2(Player player) {
+        super(player);
+    }
+
+    @Override
+    public boolean onSend(ChannelHandlerContext context, Object o, ChannelPromise promise) throws Exception {
+        if (o instanceof PacketPlayOutNamedSoundEffect) {
+            if (IStaffPlus.get().getUserManager().get(player.getUniqueId()).isVanished()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onReceive(ChannelHandlerContext context, Object o) throws Exception {
+        return true;
+    }
+}
