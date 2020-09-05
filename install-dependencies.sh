6525e19fc357e1f035cd0bac2681677c1bb4693b
David Maes <david.s.maes@gmail.com>
2020-09-05 23:50:37 +0200

    Fix build

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 declare -a VERSIONS=(
-  1.7.10-R0.1-SNAPSHOT
+  1.14.4-R0.1-SNAPSHOT
 )
 
 if [[ ! -d "lib" ]]; then
@@ -13,11 +13,12 @@ for i in "${VERSIONS[@]}"; do
   if [[ ! -f "lib/$FILE" ]]; then
     echo "Downloading $FILE..."
     curl "http://static.azoraqua.com/craftbukkit/craftbukkit-$i.jar" -s -o "$FILE"
-   
+
   fi
 
   mvn install:install-file -Dfile="$FILE" -Dpackaging=jar -DgeneratePom=true -DgroupId=org.bukkit -DartifactId=craftbukkit -Dversion="$i"
   echo "Installed $FILE to your local Maven repository."
   rm "$FILE"
 done
-rm "lib"
+cd ..
+rm -rf "lib"
