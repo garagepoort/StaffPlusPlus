1ba2e8b50a1b44dafd7f45572511624dadfdf1d8
David Maes <david.s.maes@gmail.com>
2020-09-09 20:14:38 +0200

    Merge pull request #31 from garagepoort/feature/strip_arg
    Add strip argument

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/server/command/cmd/mode/StripCmd.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/server/command/cmd/mode/StripCmd.java
@@ -1,6 +1,7 @@
 package net.shortninja.staffplus.server.command.cmd.mode;
 
 import net.shortninja.staffplus.StaffPlus;
+import net.shortninja.staffplus.player.StripService;
 import net.shortninja.staffplus.server.data.config.Messages;
 import net.shortninja.staffplus.server.data.config.Options;
 import net.shortninja.staffplus.util.MessageCoordinator;
@@ -10,13 +11,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 
 public class StripCmd extends BukkitCommand {
     private PermissionHandler permission = StaffPlus.get().permission;
     private MessageCoordinator message = StaffPlus.get().message;
     private Options options = StaffPlus.get().options;
     private Messages messages = StaffPlus.get().messages;
+    private  StripService stripService = StripService.getInstance();
 
     public StripCmd(String name) {
         super(name);
@@ -40,49 +41,11 @@ public class StripCmd extends BukkitCommand {
 
         if (targetPlayer != null) {
             if (JavaUtils.hasInventorySpace(targetPlayer)) {
-                strip(targetPlayer, 0);
-                message.send(sender, messages.strip.replace("%player%", targetPlayer.getName()), messages.prefixGeneral);
+                stripService.strip(sender, targetPlayer);
             } else
                 message.send(sender, messages.invalidArguments.replace("%usage%", usageMessage), messages.prefixGeneral);
         } else message.send(sender, messages.playerOffline, messages.prefixGeneral);
 
         return true;
-    }
-
-    private void strip(Player player, int index) {
-        ItemStack[] armor = player.getInventory().getArmorContents();
-
-        switch (index) {
-            case 0:
-                if (armor[0] != null) {
-                    player.getInventory().addItem(armor[0].clone());
-                    player.getInventory().setBoots(null);
-                }
-                break;
-            case 1:
-                if (armor[1] != null) {
-                    player.getInventory().addItem(armor[1].clone());
-                    player.getInventory().setLeggings(null);
-                }
-                break;
-            case 2:
-                if (armor[2] != null) {
-                    player.getInventory().addItem(armor[2].clone());
-                    player.getInventory().setChestplate(null);
-                }
-                break;
-            case 3:
-                if (armor[3] != null) {
-                    player.getInventory().addItem(armor[3].clone());
-                    player.getInventory().setHelmet(null);
-                }
-                break;
-            default:
-                return;
-        }
-
-        if (JavaUtils.hasInventorySpace(player) && index < 3) {
-            strip(player, index + 1);
-        }
     }
 }
