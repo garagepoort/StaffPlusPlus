e1ccd4c97bbe7a6630579545b281f3f491c164e9
David Maes <david.s.maes@gmail.com>
2020-09-17 11:12:03 +0200

    Add some logging

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/server/data/config/AutoUpdater.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/server/data/config/AutoUpdater.java
@@ -14,19 +14,28 @@ import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AutoUpdater {
 
     private static final String CONFIG_FILE = "config.yml";
 
     public static void updateConfig(StaffPlus staffPlus) {
+        staffPlus.getLogger().info("Attempting to fix configuration file...");
         FileConfiguration config = staffPlus.getConfig();
+        AtomicInteger counter = new AtomicInteger();
         loadConfig().forEach((k, v) -> {
             if (!config.contains(k, true) && !(v instanceof ConfigurationSection)) {
                 config.set(k, v);
+                counter.getAndIncrement();
             }
         });
         staffPlus.saveConfig();
+        if (counter.get() > 0) {
+            staffPlus.getLogger().info("Configuration file Fixed. [" + counter.get() + "] properties were added. Should StaffPlusPlus still have problems starting up, please compare your config with the default configuration: https://github.com/garagepoort/StaffPlusPlus/blob/master/StaffPlusCore/src/main/resources/config.yml");
+        } else {
+            staffPlus.getLogger().info("Configuration file is up to date. No fix needed");
+        }
     }
 
     private static Map<String, Object> loadConfig() {
