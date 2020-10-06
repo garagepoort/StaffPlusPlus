8535ecf1ec43c720affa329bf1e6a0bf2a4ac51c
David Maes <david.s.maes@gmail.com>
2020-10-06 15:10:47 +0200

    Add world to locations

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/teleport/config/LocationLoader.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/teleport/config/LocationLoader.java
@@ -1,7 +1,9 @@
 package net.shortninja.staffplus.staff.teleport.config;
 
 import net.shortninja.staffplus.common.config.ConfigLoader;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 
 import java.util.Collections;
@@ -23,11 +25,12 @@ public class LocationLoader extends ConfigLoader<Map<String, Location>> {
         for (String identifier : locationsConfig.getKeys(false)) {
             String locationString = config.getString("locations." + identifier);
             String[] points = locationString.split(";");
-            if (points.length != 3) {
-                throw new RuntimeException("Invalid locations configuration. Make sure your location points are in format x;y;z");
+            if (points.length < 3) {
+                throw new RuntimeException("Invalid locations configuration. Make sure your location points are in format x;y;z;worldname");
             }
 
-            Location location = new Location(null, parseDouble(points[0]), parseDouble(points[1]), parseDouble(points[2]));
+            World world = points.length == 4 ? Bukkit.getWorld(points[3]) : Bukkit.getWorlds().get(0);
+            Location location = new Location(world, parseDouble(points[0]), parseDouble(points[1]), parseDouble(points[2]));
             locations.put(identifier, location);
         }
         return locations;
