f2d8b18e65544690b68ba8ca6aba4490f804b21a
David Maes <david.maes@kbc.be>
2020-10-12 11:42:14 +0200

    Cleanup ioccontainer

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/ban/database/AbstractSqlBansRepository.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/ban/database/AbstractSqlBansRepository.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public abstract class AbstractSqlBansRepository implements BansRepository, IocContainer.Repository {
+public abstract class AbstractSqlBansRepository implements BansRepository {
 
     private final PlayerManager playerManager;
 
--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/warn/database/AbstractSqlWarnRepository.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/warn/database/AbstractSqlWarnRepository.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public abstract class AbstractSqlWarnRepository implements WarnRepository, IocContainer.Repository {
+public abstract class AbstractSqlWarnRepository implements WarnRepository {
 
     private final PlayerManager playerManager;
 
