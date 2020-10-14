a0b945649cf658268f226f0e35cf7d14e32e2969
David Maes <david.s.maes@gmail.com>
2020-10-14 23:12:56 +0200

    Merge pull request #116 from garagepoort/feature/warning_notifications
    Feature/warning notifications

---
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/warn/gui/WarningItemBuilder.java
@@ -0,0 +1,41 @@
+package net.shortninja.staffplus.staff.warn.gui;
+
+import net.shortninja.staffplus.IocContainer;
+import net.shortninja.staffplus.StaffPlus;
+import net.shortninja.staffplus.staff.warn.Warning;
+import net.shortninja.staffplus.util.lib.hex.Items;
+import org.bukkit.inventory.ItemStack;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.shortninja.staffplus.util.lib.JavaUtils.formatLines;
+
+public class WarningItemBuilder {
+
+    public static ItemStack build(Warning warning) {
+        List<String> lore = new ArrayList<String>();
+
+        lore.add("&bSeverity: " + warning.getSeverity());
+        lore.add("&bTimeStamp: " + warning.getTimestamp().format(DateTimeFormatter.ofPattern("dd/MM/yyyy-HH:mm")));
+        if (IocContainer.getOptions().warningConfiguration.isShowIssuer()) {
+            lore.add("&bIssuer: " + warning.getIssuerName());
+        }
+
+        lore.add("&bReason:");
+        for (String line : formatLines(warning.getReason(), 30)) {
+            lore.add("  &b" + line);
+        }
+
+        String reason = warning.getReason().length() > 12 ? warning.getReason().substring(0, 9) + "..." : warning.getReason();
+        ItemStack item = Items.editor(Items.createSkull(warning.getName())).setAmount(1)
+            .setName("&b" + reason)
+            .setLore(lore)
+            .build();
+
+        return StaffPlus.get().versionProtocol.addNbtString(item, String.valueOf(warning.getId()));
+    }
+
+
+}
