c83f3656071714d2285e558c2e70a36d2de9b2da
David Maes <david.s.maes@gmail.com>
2020-10-14 23:12:56 +0200

    Merge pull request #116 from garagepoort/feature/warning_notifications
    Feature/warning notifications

---
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/warn/gui/MyWarningsGui.java
@@ -0,0 +1,46 @@
+package net.shortninja.staffplus.staff.warn.gui;
+
+import net.shortninja.staffplus.IocContainer;
+import net.shortninja.staffplus.player.attribute.gui.PagedGui;
+import net.shortninja.staffplus.unordered.IAction;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyWarningsGui extends PagedGui {
+
+    public MyWarningsGui(Player player, String title, int page) {
+        super(player, title, page);
+    }
+
+    @Override
+    protected void getNextUi(Player player, String title, int page) {
+        new MyWarningsGui(player, title, page);
+    }
+
+    @Override
+    public IAction getAction() {
+        return new IAction() {
+            @Override
+            public void click(Player player, ItemStack item, int slot) {
+                // Do nothing for now
+            }
+
+            @Override
+            public boolean shouldClose() {
+                return false;
+            }
+        };
+    }
+
+    @Override
+    public List<ItemStack> getItems(Player player, int offset, int amount) {
+        return IocContainer.getWarnService()
+                .getWarnings(player.getUniqueId(), offset, amount)
+                .stream()
+                .map(WarningItemBuilder::build)
+                .collect(Collectors.toList());
+    }
+}
---
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/util/database/migrations/common/V24_AlterWarningTableAddTimestampMigration.java
@@ -0,0 +1,15 @@
+package net.shortninja.staffplus.util.database.migrations.common;
+
+import net.shortninja.staffplus.util.database.migrations.Migration;
+
+public class V24_AlterWarningTableAddTimestampMigration implements Migration {
+    @Override
+    public String getStatement() {
+        return "ALTER TABLE sp_warnings ADD COLUMN timestamp BIGINT NOT NULL DEFAULT 1602696006000;";
+    }
+
+    @Override
+    public int getVersion() {
+        return 24;
+    }
+}
