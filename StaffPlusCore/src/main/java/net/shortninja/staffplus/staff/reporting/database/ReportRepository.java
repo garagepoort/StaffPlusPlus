c5c7472189f28c94de82fb20d5cf0051bbdc7993
David Maes <david.s.maes@gmail.com>
2020-10-15 23:09:12 +0200

    Merge pull request #118 from garagepoort/feature/notify_reporter
    Feature/notify reporter

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/reporting/database/ReportRepository.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/reporting/database/ReportRepository.java
@@ -14,8 +14,6 @@ public interface ReportRepository {
 
     List<Report> getUnresolvedReports(int offset, int amount);
 
-    List<Report> getUnresolvedReports(UUID playerUuid, int offset, int amount);
-
     void removeReports(UUID playerUuid);
 
     Optional<Report> findOpenReport(int reportId);
@@ -25,6 +23,10 @@ public interface ReportRepository {
     void updateReport(Report report);
 
     List<Report> getAssignedReports(UUID staffUuid, int offset, int amount);
+
+    List<Report> getMyReports(UUID staffUuid, int offset, int amount);
+
+    List<Report> getMyReports(UUID reporterUuid);
 
     List<Report> getClosedReports(int offset, int amount);
 }
