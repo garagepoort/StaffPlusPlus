6526a6573abbebfc11bab2dc12e6d24e48927e8d
David Maes <david.s.maes@gmail.com>
2020-10-20 14:06:38 +0200

    Add teleport back cmd

---
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/teleport/cmd/TeleportBackCmd.java
@@ -0,0 +1,74 @@
+package net.shortninja.staffplus.staff.teleport.cmd;
+
+import net.shortninja.staffplus.IocContainer;
+import net.shortninja.staffplus.player.SppPlayer;
+import net.shortninja.staffplus.server.command.AbstractCmd;
+import net.shortninja.staffplus.server.command.PlayerRetrievalStrategy;
+import net.shortninja.staffplus.server.command.arguments.ArgumentType;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static net.shortninja.staffplus.server.command.arguments.ArgumentType.HEALTH;
+import static net.shortninja.staffplus.server.command.arguments.ArgumentType.STRIP;
+
+public class TeleportBackCmd extends AbstractCmd {
+
+    public TeleportBackCmd(String name) {
+        super(name, IocContainer.getOptions().permissionTeleport);
+    }
+
+    @Override
+    protected boolean executeCmd(CommandSender sender, String alias, String[] args, SppPlayer targetPlayer) {
+        IocContainer.getTeleportService().teleportPlayerBack(targetPlayer.getPlayer());
+        return true;
+    }
+
+    @Override
+    protected List<ArgumentType> getPostExecutionSppArguments() {
+        return Arrays.asList(STRIP, HEALTH);
+    }
+
+    @Override
+    protected Optional<String> getPlayerName(CommandSender sender, String[] args) {
+        return Optional.ofNullable(args[0]);
+    }
+
+    @Override
+    protected int getMinimumArguments(CommandSender sender, String[] args) {
+        return 2;
+    }
+
+    @Override
+    protected boolean isDelayable() {
+        return false;
+    }
+
+    @Override
+    protected PlayerRetrievalStrategy getPlayerRetrievalStrategy() {
+        return PlayerRetrievalStrategy.ONLINE;
+    }
+
+    @Override
+    protected boolean canBypass(Player player) {
+        return permission.has(player, options.permissionTeleportBypass);
+    }
+
+    @Override
+    public List<String> tabComplete(CommandSender sender, String alias, String[] args) throws IllegalArgumentException {
+        if (args.length == 1) {
+            return Bukkit.getOnlinePlayers().stream()
+                .map(HumanEntity::getName)
+                .filter(s -> args[0].isEmpty() || s.contains(args[0]))
+                .collect(Collectors.toList());
+        }
+
+        return getSppArgumentsSuggestions(sender, args);
+    }
+}
