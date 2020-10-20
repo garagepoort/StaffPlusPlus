2e127c2da0d3e9e760e356cd1def5e8233454d51
David Maes <david.s.maes@gmail.com>
2020-10-20 15:45:58 +0200

    Merge pull request #120 from garagepoort/feature/teleport-back
    Feature/teleport back

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/teleport/TeleportService.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/teleport/TeleportService.java
@@ -1,14 +1,18 @@
 package net.shortninja.staffplus.staff.teleport;
 
 import net.shortninja.staffplus.IocContainer;
+import net.shortninja.staffplus.common.exceptions.BusinessException;
 import net.shortninja.staffplus.server.data.config.Options;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.*;
+
 public class TeleportService {
 
-    private static TeleportService instance;
+    private static Map<Player, Deque<Location>> previousLocations = new HashMap<>();
+
     private final Options options;
 
     public TeleportService(Options options) {
@@ -17,24 +21,41 @@ public class TeleportService {
 
     public void teleportPlayer(CommandSender commandSender, Player targetPlayer, String locationId) {
         if (!options.locations.containsKey(locationId)) {
-            commandSender.sendMessage("No location found with name: " + locationId);
-            return;
+            throw new BusinessException("&CNo location found with name: " + locationId);
         }
 
         Location location = options.locations.get(locationId);
+        addPreviousLocation(targetPlayer);
         targetPlayer.teleport(location);
-        IocContainer.getMessage().send(commandSender, targetPlayer.getName() + " teleported to " + locationId, IocContainer.getMessages().prefixGeneral);
+        IocContainer.getMessage().send(commandSender, "&6" + targetPlayer.getName() + " teleported to " + locationId, IocContainer.getMessages().prefixGeneral);
     }
 
 
     public void teleportSelf(Player targetPlayer, Location location) {
+        addPreviousLocation(targetPlayer);
         targetPlayer.teleport(location);
-        IocContainer.getMessage().send(targetPlayer, "You have been teleported", IocContainer.getMessages().prefixGeneral);
+        IocContainer.getMessage().send(targetPlayer, "&6You have been teleported", IocContainer.getMessages().prefixGeneral);
     }
 
     public void teleportToPlayer(Player sourcePlayer, Player targetPlayer) {
         Location location = targetPlayer.getLocation();
         location.setWorld(targetPlayer.getWorld());
+        addPreviousLocation(sourcePlayer);
         sourcePlayer.teleport(location);
+    }
+
+    public void teleportPlayerBack(Player player) {
+        if(!previousLocations.containsKey(player) || previousLocations.get(player).isEmpty()) {
+            throw new BusinessException("&CUnable to teleport player back, no previous locations found");
+        }
+        Location previousLocation = previousLocations.get(player).pop();
+        player.teleport(previousLocation);
+    }
+
+    private void addPreviousLocation(Player player) {
+        if(!previousLocations.containsKey(player)) {
+            previousLocations.put(player, new ArrayDeque<>());
+        }
+        previousLocations.get(player).push(player.getLocation());
     }
 }
