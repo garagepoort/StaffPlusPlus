aac10d57531ca11b4d10e88db47287ce9a5022d2
David Maes <david.s.maes@gmail.com>
2020-11-15 00:46:45 +0100

    Warnings through gui

---
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/examine/SeverityLevelSelectGui.java
@@ -0,0 +1,33 @@
+package net.shortninja.staffplus.staff.examine;
+
+import net.shortninja.staffplus.IocContainer;
+import net.shortninja.staffplus.player.attribute.gui.AbstractGui;
+import net.shortninja.staffplus.server.data.config.Options;
+import net.shortninja.staffplus.session.SessionManager;
+import net.shortninja.staffplus.staff.warn.config.WarningSeverityConfiguration;
+import net.shortninja.staffplus.unordered.IAction;
+import org.bukkit.entity.Player;
+
+public class SeverityLevelSelectGui extends AbstractGui {
+    private static final int SIZE = 54;
+
+    private final SessionManager sessionManager = IocContainer.getSessionManager();
+    private final Options options = IocContainer.getOptions();
+
+    public SeverityLevelSelectGui(Player player, String title, Player targetPlayer) {
+        super(SIZE, title);
+
+        IAction selectAction = new WarnPlayerAction(targetPlayer);
+        int count = 0;
+        for (WarningSeverityConfiguration severityConfiguration : options.warningConfiguration.getSeverityLevels()) {
+            setItem(count, SeverityLevelItemBuilder.build(severityConfiguration), selectAction);
+            count++;
+        }
+
+
+        player.closeInventory();
+        player.openInventory(getInventory());
+        sessionManager.get(player.getUniqueId()).setCurrentGui(this);
+    }
+
+}
