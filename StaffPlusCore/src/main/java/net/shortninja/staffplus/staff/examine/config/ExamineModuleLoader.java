06bd670ab984665d866e579430cdae5458b82177
David Maes <david.s.maes@gmail.com>
2021-01-09 22:11:01 +0100

    Update examine permissions

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/examine/config/ExamineModuleLoader.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/examine/config/ExamineModuleLoader.java
@@ -9,9 +9,11 @@ public class ExamineModuleLoader extends ConfigLoader<ExamineConfiguration> {
     protected ExamineConfiguration load(FileConfiguration config) {
         String permissionExamine = config.getString("permissions.examine");
         String permissionExamineInventoryInteraction = config.getString("permissions.examine-inventory-interaction");
+        String permissionExamineInventoryInteractionOffline = config.getString("permissions.examine-inventory-interaction.offline");
         String permissionExamineViewInventory = config.getString("permissions.examine-view-inventory");
+        String permissionExamineViewInventoryOffline = config.getString("permissions.examine-view-inventory.offline");
         String commandExamine = config.getString("commands.examine");
 
-        return new ExamineConfiguration(permissionExamine, permissionExamineInventoryInteraction, permissionExamineViewInventory, commandExamine );
+        return new ExamineConfiguration(permissionExamine, permissionExamineInventoryInteraction, permissionExamineInventoryInteractionOffline, permissionExamineViewInventory, permissionExamineViewInventoryOffline, commandExamine );
     }
 }
