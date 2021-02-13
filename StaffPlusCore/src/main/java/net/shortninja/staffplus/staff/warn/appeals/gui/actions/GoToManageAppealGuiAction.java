284df6d94bb0b4713383503ae4d61fcca10d3f52
David Maes <david.s.maes@gmail.com>
2021-02-13 10:36:42 +0100

    Fix appeal bugs

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/warn/appeals/gui/actions/GoToManageAppealGuiAction.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/warn/appeals/gui/actions/GoToManageAppealGuiAction.java
@@ -3,6 +3,7 @@ package net.shortninja.staffplus.staff.warn.appeals.gui.actions;
 import net.shortninja.staffplus.player.attribute.gui.AbstractGui;
 import net.shortninja.staffplus.staff.warn.appeals.Appeal;
 import net.shortninja.staffplus.staff.warn.appeals.gui.ManageAppealGui;
+import net.shortninja.staffplus.staff.warn.warnings.Warning;
 import net.shortninja.staffplus.unordered.IAction;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -11,16 +12,18 @@ import java.util.function.Supplier;
 
 public class GoToManageAppealGuiAction implements IAction {
     private final Supplier<AbstractGui> previousGuiSupplier;
+    private final Warning warning;
     private final Appeal appeal;
 
-    public GoToManageAppealGuiAction(Appeal appeal, Supplier<AbstractGui> previousGuiSupplier) {
+    public GoToManageAppealGuiAction(Warning warning, Appeal appeal, Supplier<AbstractGui> previousGuiSupplier) {
+        this.warning = warning;
         this.appeal = appeal;
         this.previousGuiSupplier = previousGuiSupplier;
     }
 
     @Override
     public void click(Player player, ItemStack item, int slot) {
-        new ManageAppealGui(player, "Manage warning appeal", appeal, previousGuiSupplier)
+        new ManageAppealGui(player, "Manage warning appeal", warning, appeal, previousGuiSupplier)
             .show(player);
     }
 
