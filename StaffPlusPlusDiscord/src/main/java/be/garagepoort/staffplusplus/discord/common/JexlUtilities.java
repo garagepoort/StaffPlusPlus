f52e1ddae53ab06129a58262daf1263c5d682a45
David Maes <david.s.maes@gmail.com>
2021-02-19 08:25:42 +0100

    Add report location to discord

--- StaffPlusPlusDiscord/src/main/java/be/garagepoort/staffplusplus/discord/common/JexlUtilities.java
+++ StaffPlusPlusDiscord/src/main/java/be/garagepoort/staffplusplus/discord/common/JexlUtilities.java
@@ -1,5 +1,8 @@
 package be.garagepoort.staffplusplus.discord.common;
 
+import net.shortninja.staffplus.util.lib.JavaUtils;
+import org.bukkit.Location;
+
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -10,5 +13,14 @@ public class JexlUtilities {
     public static String parseTime(ZonedDateTime zonedDateTime) {
         LocalDateTime endDateTime = LocalDateTime.ofInstant(zonedDateTime.toInstant(), ZoneOffset.UTC);
         return endDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+
+    public static String parseLocation(String serverName, Location location) {
+        serverName = serverName == null ? "Unknown server" : serverName;
+        return serverName + "\\\\n" + location.getWorld().getName() + " | " + JavaUtils.serializeLocation(location);
+    }
+
+    public static String parseLocation(Location location) {
+        return location.getWorld().getName() + " | " + JavaUtils.serializeLocation(location);
     }
 }
