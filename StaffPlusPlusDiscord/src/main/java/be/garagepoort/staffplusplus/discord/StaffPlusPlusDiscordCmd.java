cf9b26c25df91ebefa1990cf28acea3008785e30
David Maes <david.s.maes@gmail.com>
2021-02-19 14:08:22 +0100

    Update templates with comment and add permission for discord reload

--- StaffPlusPlusDiscord/src/main/java/be/garagepoort/staffplusplus/discord/StaffPlusPlusDiscordCmd.java
+++ StaffPlusPlusDiscord/src/main/java/be/garagepoort/staffplusplus/discord/StaffPlusPlusDiscordCmd.java
@@ -7,7 +7,7 @@ import org.bukkit.command.CommandSender;
 public class StaffPlusPlusDiscordCmd implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String alias, String[] args) {
-        if (args.length == 1 && "reload".equalsIgnoreCase(args[0])) {
+        if (args.length == 1 && "reload".equalsIgnoreCase(args[0]) && commandSender.hasPermission("staffplusplusdiscord.reload")) {
             StaffPlusPlusDiscord.get().reload();
             commandSender.sendMessage("[S++ Discord] Configuration reloaded");
         } else {
