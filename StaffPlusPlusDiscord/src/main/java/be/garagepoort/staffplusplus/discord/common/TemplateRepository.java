8554dd11e6e399101c87175d92651182da2c204d
Jonathan Stevens <jonathan@videndum.studio>
2021-03-06 12:39:17 +0000

    feat: new templates
    This commit add's new templates, and a new way to handle them. Allows for you to have multiple templates installed at any time and quickly and easily switch between them.

--- StaffPlusPlusDiscord/src/main/java/be/garagepoort/staffplusplus/discord/common/TemplateRepository.java
+++ StaffPlusPlusDiscord/src/main/java/be/garagepoort/staffplusplus/discord/common/TemplateRepository.java
@@ -15,45 +15,48 @@ public class TemplateRepository {
 
     public TemplateRepository(StaffPlusPlusDiscord plugin, FileConfiguration config) {
         boolean replaceTemplates = config.getBoolean("StaffPlusPlusDiscord.updateTemplates", true);
-        plugin.saveResource("discordtemplates/reports/report-created.json", replaceTemplates);
+        String templatePack = config.getString("StaffPlusPlusDiscord.templatePack", "default");
         boolean testFile = plugin.getDataFolder().exists("discordtemplates" + separator + "template_version.yml")
         if (replaceTemplates || !testFile) {
-        plugin.saveResource("discordtemplates/reports/report-rejected.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/reports/report-reopened.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/warnings/warning-created.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/warnings/threshold-reached.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/warnings/appeals/appeal-created.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/warnings/appeals/appeal-approved.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/warnings/appeals/appeal-rejected.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/bans/banned.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/bans/unbanned.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/mutes/muted.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/mutes/unmuted.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/kicks/kicked.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/altdetects/detected.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/staffmode/enter-staffmode.json", replaceTemplates);
-        plugin.saveResource("discordtemplates/staffmode/exit-staffmode.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "reports" + separator + "report-created.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "reports" + separator + "report-accepted.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "reports" + separator + "report-resolved.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "reports" + separator + "report-rejected.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "reports" + separator + "report-reopened.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "warnings" + separator + "warning-created.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "warnings" + separator + "threshold-reached.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "warnings" + separator + "appeals" + separator + "appeal-created.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "warnings" + separator + "appeals" + separator + "appeal-approved.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "warnings" + separator + "appeals" + separator + "appeal-rejected.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "bans" + separator + "banned.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "bans" + separator + "unbanned.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "mutes" + separator + "muted.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "mutes" + separator + "unmuted.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "kicks" + separator + "kicked.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "altdetects" + separator + "detected.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "staffmode" + separator + "enter-staffmode.json", replaceTemplates);
+            plugin.saveResource("discordtemplates" + separator + templatePack + separator + "staffmode" + separator + "exit-staffmode.json", replaceTemplates);
             plugin.saveResource("discordtemplates" + separator + "template_version.yml", replaceTemplates);
         }
 
-        templates.put("reports/report-created", Utils.readTemplate(PATH_PREFIX + "reports" + separator + "report-created.json"));
-        templates.put("reports/report-accepted", Utils.readTemplate(PATH_PREFIX + "reports" + separator + "report-accepted.json"));
-        templates.put("reports/report-resolved", Utils.readTemplate(PATH_PREFIX + "reports" + separator + "report-resolved.json"));
-        templates.put("reports/report-rejected", Utils.readTemplate(PATH_PREFIX + "reports" + separator + "report-rejected.json"));
-        templates.put("reports/report-reopened", Utils.readTemplate(PATH_PREFIX + "reports" + separator + "report-reopened.json"));
-        templates.put("warnings/warning-created", Utils.readTemplate(PATH_PREFIX + "warnings" + separator + "warning-created.json"));
-        templates.put("warnings/threshold-reached", Utils.readTemplate(PATH_PREFIX + "warnings" + separator + "threshold-reached.json"));
-        templates.put("warnings/appeals/appeal-created", Utils.readTemplate(PATH_PREFIX + "warnings" + separator + "appeals" + separator + "appeal-created.json"));
-        templates.put("warnings/appeals/appeal-approved", Utils.readTemplate(PATH_PREFIX + "warnings" + separator + "appeals" + separator + "appeal-approved.json"));
-        templates.put("warnings/appeals/appeal-rejected", Utils.readTemplate(PATH_PREFIX + "warnings" + separator + "appeals" + separator + "appeal-rejected.json"));
-        templates.put("bans/banned", Utils.readTemplate(PATH_PREFIX + "bans" + separator + "banned.json"));
-        templates.put("bans/unbanned", Utils.readTemplate(PATH_PREFIX + "bans" + separator + "unbanned.json"));
-        templates.put("mutes/muted", Utils.readTemplate(PATH_PREFIX + "mutes" + separator + "muted.json"));
-        templates.put("mutes/unmuted", Utils.readTemplate(PATH_PREFIX + "mutes" + separator + "unmuted.json"));
-        templates.put("kicks/kicked", Utils.readTemplate(PATH_PREFIX + "kicks" + separator + "kicked.json"));
-        templates.put("altdetects/detected", Utils.readTemplate(PATH_PREFIX + "altdetects" + separator + "detected.json"));
-        templates.put("staffmode/enter-staffmode", Utils.readTemplate(PATH_PREFIX + "staffmode" + separator + "enter-staffmode.json"));
-        templates.put("staffmode/exit-staffmode", Utils.readTemplate(PATH_PREFIX + "staffmode" + separator + "exit-staffmode.json"));
+        templates.put("reports/report-created", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "reports" + separator + "report-created.json"));
+        templates.put("reports/report-accepted", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "reports" + separator + "report-accepted.json"));
+        templates.put("reports/report-resolved", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "reports" + separator + "report-resolved.json"));
+        templates.put("reports/report-rejected", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "reports" + separator + "report-rejected.json"));
+        templates.put("reports/report-reopened", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "reports" + separator + "report-reopened.json"));
+        templates.put("warnings/warning-created", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "warnings" + separator + "warning-created.json"));
+        templates.put("warnings/threshold-reached", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "warnings" + separator + "threshold-reached.json"));
+        templates.put("warnings/appeals/appeal-created", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "warnings" + separator + "appeals" + separator + "appeal-created.json"));
+        templates.put("warnings/appeals/appeal-approved", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "warnings" + separator + "appeals" + separator + "appeal-approved.json"));
+        templates.put("warnings/appeals/appeal-rejected", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "warnings" + separator + "appeals" + separator + "appeal-rejected.json"));
+        templates.put("bans/banned", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "bans" + separator + "banned.json"));
+        templates.put("bans/unbanned", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "bans" + separator + "unbanned.json"));
+        templates.put("mutes/muted", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "mutes" + separator + "muted.json"));
+        templates.put("mutes/unmuted", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "mutes" + separator + "unmuted.json"));
+        templates.put("kicks/kicked", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "kicks" + separator + "kicked.json"));
+        templates.put("altdetects/detected", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "altdetects" + separator + "detected.json"));
+        templates.put("staffmode/enter-staffmode", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "staffmode" + separator + "enter-staffmode.json"));
+        templates.put("staffmode/exit-staffmode", Utils.readTemplate(PATH_PREFIX + templatePack + separator + "staffmode" + separator + "exit-staffmode.json"));
     }
 
     public String getTemplate(String key) {
