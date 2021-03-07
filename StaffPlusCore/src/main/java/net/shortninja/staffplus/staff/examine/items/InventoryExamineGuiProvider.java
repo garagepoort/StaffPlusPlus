62db0393688586d59d5455f0019c4c3a6a6299ee
David Maes <david.s.maes@gmail.com>
2021-03-08 00:32:21 +0100

    Permission based enable/disable commands

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/examine/items/InventoryExamineGuiProvider.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/examine/items/InventoryExamineGuiProvider.java
@@ -1,33 +1,27 @@
 package net.shortninja.staffplus.staff.examine.items;
 
+import net.shortninja.staffplus.common.IAction;
+import net.shortninja.staffplus.common.Items;
 import net.shortninja.staffplus.player.SppPlayer;
-import net.shortninja.staffplus.server.data.config.Messages;
 import net.shortninja.staffplus.server.data.config.Options;
 import net.shortninja.staffplus.staff.chests.ChestGUI;
 import net.shortninja.staffplus.staff.chests.ChestGuiType;
 import net.shortninja.staffplus.staff.examine.ExamineGui;
 import net.shortninja.staffplus.staff.examine.ExamineGuiItemProvider;
-import net.shortninja.staffplus.staff.mode.config.modeitems.examine.ExamineModeConfiguration;
-import net.shortninja.staffplus.common.IAction;
 import net.shortninja.staffplus.util.PermissionHandler;
 import net.shortninja.staffplus.util.factory.InventoryFactory;
-import net.shortninja.staffplus.common.Items;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class InventoryExamineGuiProvider implements ExamineGuiItemProvider {
 
-    private final Messages messages;
-    private final ExamineModeConfiguration examineModeConfiguration;
     private final Options options;
     private PermissionHandler permissionHandler;
 
-    public InventoryExamineGuiProvider(Messages messages, Options options, PermissionHandler permissionHandler) {
-        this.messages = messages;
+    public InventoryExamineGuiProvider(Options options, PermissionHandler permissionHandler) {
         this.options = options;
         this.permissionHandler = permissionHandler;
-        examineModeConfiguration = this.options.modeConfiguration.getExamineModeConfiguration();
     }
 
     @Override
@@ -42,9 +36,9 @@ public class InventoryExamineGuiProvider implements ExamineGuiItemProvider {
             public void click(Player player, ItemStack item, int slot) {
                 ChestGUI chestGUI;
                 if (target.isOnline()) {
-                    chestGUI = new ChestGUI(target, target.getPlayer().getPlayer().getInventory(), 54, ChestGuiType.PLAYER_INVENTORY_EXAMINE, permissionHandler.has(player, options.examineConfiguration.getPermissionExamineInventoryInteraction()));
+                    chestGUI = new ChestGUI(target, target.getPlayer().getPlayer().getInventory(), 45, ChestGuiType.PLAYER_INVENTORY_EXAMINE, permissionHandler.has(player, options.examineConfiguration.getPermissionExamineInventoryInteraction()));
                 } else {
-                    chestGUI = new ChestGUI(target, InventoryFactory.loadInventoryOffline(staff, target), 54, ChestGuiType.PLAYER_INVENTORY_EXAMINE, permissionHandler.has(player, options.examineConfiguration.getPermissionExamineInventoryInteractionOffline()));
+                    chestGUI = new ChestGUI(target, InventoryFactory.loadInventoryOffline(staff, target), 45, ChestGuiType.PLAYER_INVENTORY_EXAMINE, permissionHandler.has(player, options.examineConfiguration.getPermissionExamineInventoryInteractionOffline()));
                 }
                 fillEmptyPlaces(chestGUI);
                 chestGUI.show(staff);
@@ -58,7 +52,7 @@ public class InventoryExamineGuiProvider implements ExamineGuiItemProvider {
     }
 
     private void fillEmptyPlaces(ChestGUI chestGUI) {
-        for (int i = 41; i <= 53; i++) {
+        for (int i = 41; i <= 44; i++) {
             chestGUI.setItem(i, Items.createRedColoredGlass("Not a player slot", ""), null);
         }
     }
