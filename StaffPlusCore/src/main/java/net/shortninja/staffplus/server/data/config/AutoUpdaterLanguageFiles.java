85c98737de52235f619f8460962ebc2698e4eef4
David Maes <david.s.maes@gmail.com>
2021-03-12 14:18:20 +0100

    Fix updater resetting config file when the file cannot be parsed

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/server/data/config/AutoUpdaterLanguageFiles.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/server/data/config/AutoUpdaterLanguageFiles.java
@@ -1,82 +1,59 @@
 package net.shortninja.staffplus.server.data.config;
 
-import com.google.common.base.Charsets;
 import net.shortninja.staffplus.StaffPlus;
+import net.shortninja.staffplus.common.exceptions.ConfigurationException;
 import net.shortninja.staffplus.server.data.file.LanguageFile;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class AutoUpdaterLanguageFiles {
+public class AutoUpdaterLanguageFiles extends AbstractConfigUpdater {
 
-    public static void updateConfig(StaffPlus staffPlus) {
+    public static boolean updateConfig(StaffPlus staffPlus) {
         for (String langFile : LanguageFile.LANG_FILES) {
             try {
-                updateConfig(staffPlus, langFile + ".yml");
+                boolean langUpdate = updateConfig(staffPlus, langFile + ".yml");
+                if (!langUpdate) {
+                    return langUpdate;
+                }
             } catch (IOException e) {
                 staffPlus.getLogger().severe("Failed to update language file: " + langFile);
                 e.printStackTrace();
+                return false;
             }
         }
+        return true;
     }
 
-    private static void updateConfig(StaffPlus staffPlus, String languageFile) throws IOException {
-        staffPlus.getLogger().info("Attempting to fix language file...");
-
-        File langFile = new File(StaffPlus.get().getDataFolder() + "/lang/", languageFile);
-        FileConfiguration lang = YamlConfiguration.loadConfiguration(langFile);
-        AtomicInteger counter = new AtomicInteger();
-
-        loadConfig(languageFile).forEach((k, v) -> {
-            if (!lang.contains(k, true) && !(v instanceof ConfigurationSection)) {
-                lang.set(k, v);
-                counter.getAndIncrement();
-            }
-        });
-        lang.save(langFile);
-        if (counter.get() > 0) {
-            staffPlus.getLogger().info("Language file " + languageFile + " Fixed. [" + counter.get() + "] properties were added.");
-        } else {
-            staffPlus.getLogger().info("Language file " + languageFile + " is up to date. No fix needed");
-        }
-    }
-
-    private static Map<String, Object> loadConfig(String filename) {
-        Map<String, Object> configurations = new HashMap<>();
-        InputStream defConfigStream = getResource("lang/" + filename);
-        if (defConfigStream != null) {
-            YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, Charsets.UTF_8));
-            Set<String> keys = yamlConfiguration.getKeys(true);
-            keys.forEach((k) -> {
-                configurations.put(k, yamlConfiguration.get(k));
-            });
-        }
-        return configurations;
-    }
-
-    private static InputStream getResource(String filename) {
+    private static boolean updateConfig(StaffPlus staffPlus, String languageFile) throws IOException {
         try {
-            URL url = AutoUpdaterLanguageFiles.class.getClassLoader().getResource(filename);
-            if (url == null) {
-                return null;
+            staffPlus.getLogger().info("Attempting to fix language file [" + languageFile + "]");
+            FileConfiguration lang = getConfigFile(languageFile);
+            AtomicInteger counter = new AtomicInteger();
+            File langFile = new File(StaffPlus.get().getDataFolder() + "/lang/", languageFile);
+
+            loadConfig(languageFile).forEach((k, v) -> {
+                if (!lang.contains(k, true) && !(v instanceof ConfigurationSection)) {
+                    lang.set(k, v);
+                    counter.getAndIncrement();
+                }
+            });
+            lang.save(langFile);
+            if (counter.get() > 0) {
+                staffPlus.getLogger().info("Language file " + languageFile + " Fixed. [" + counter.get() + "] properties were added.");
             } else {
-                URLConnection connection = url.openConnection();
-                connection.setUseCaches(false);
-                return connection.getInputStream();
+                staffPlus.getLogger().info("Language file " + languageFile + " is up to date. No fix needed");
             }
-        } catch (IOException var4) {
-            return null;
+            return true;
+        } catch (InvalidConfigurationException | IOException | ConfigurationException e) {
+            staffPlus.getLogger().severe("Language file is INVALID!!! Disabling StaffPlusPlus!");
+            staffPlus.getLogger().severe("Full error [" + e.getMessage() + "]");
+            return false;
         }
     }
+
 }
