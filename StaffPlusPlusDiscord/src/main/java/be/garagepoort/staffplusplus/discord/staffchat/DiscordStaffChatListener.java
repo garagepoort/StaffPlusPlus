a1d1b569d38d10a99953db322c20c9bab35d4adc
David Maes <david.s.maes@gmail.com>
2021-03-14 23:23:50 +0100

    Fix discord processed message

--- StaffPlusPlusDiscord/src/main/java/be/garagepoort/staffplusplus/discord/staffchat/DiscordStaffChatListener.java
+++ StaffPlusPlusDiscord/src/main/java/be/garagepoort/staffplusplus/discord/staffchat/DiscordStaffChatListener.java
@@ -3,9 +3,7 @@ package be.garagepoort.staffplusplus.discord.staffchat;
 import be.garagepoort.staffplusplus.discord.StaffPlusPlusDiscord;
 import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.api.Subscribe;
-import github.scarsz.discordsrv.api.events.DiscordGuildMessagePreProcessEvent;
-import github.scarsz.discordsrv.dependencies.emoji.EmojiParser;
-import github.scarsz.discordsrv.dependencies.jda.api.entities.Message;
+import github.scarsz.discordsrv.api.events.DiscordGuildMessagePostProcessEvent;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
 import net.shortninja.staffplusplus.IStaffPlus;
@@ -23,13 +21,13 @@ public class DiscordStaffChatListener {
     }
 
     @Subscribe
-    public void onDiscordChat(DiscordGuildMessagePreProcessEvent event) {
+    public void onDiscordChat(DiscordGuildMessagePostProcessEvent event) {
         if (event.getChannel().equals(getDiscordChannel())) {
             event.setCancelled(true); // Cancel this message from getting sent to global chat.
 
             // Handle this on the main thread next tick.
             plugin.getServer().getScheduler().runTask(plugin, () ->
-                submitMessageFromDiscord(event.getAuthor(), event.getMessage())
+                submitMessageFromDiscord(event.getAuthor(), event.getProcessedMessage())
             );
         }
     }
@@ -42,20 +40,10 @@ public class DiscordStaffChatListener {
         return destinationTextChannelForGameChannelName;
     }
 
-    public void submitMessageFromDiscord(User author, Message message)
+    public void submitMessageFromDiscord(User author, String message)
     {
         Objects.requireNonNull(author, "author");
         Objects.requireNonNull(message, "message");
-
-        String text = EmojiParser.parseToAliases(message.getContentStripped());
-        String format = plugin.getConfig().getString("StaffPlusPlusDiscord.staffchat.message-format");
-
-        String authorNickname = message.getGuild().getMember(author).getEffectiveName();
-        String messageText = format
-            .replace("%text%", text)
-            .replace("%sender%", author.getName())
-            .replace("%sender_nickname%", authorNickname);
-
-        staffPlus.getStaffChatService().sendMessage(author.getName(), messageText);
+        staffPlus.getStaffChatService().sendMessage(message);
     }
 }
