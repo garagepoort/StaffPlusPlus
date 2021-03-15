63e9335de144c3c844c9bd5f7735707b94ea49fd
David Maes <david.s.maes@gmail.com>
2021-03-15 21:51:53 +0100

    Add placeholder

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/common/confirmation/ConfirmationConfig.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/common/confirmation/ConfirmationConfig.java
@@ -2,6 +2,8 @@ package net.shortninja.staffplus.common.confirmation;
 
 import net.shortninja.staffplus.staff.mode.item.ConfirmationType;
 
+import java.util.Map;
+
 public class ConfirmationConfig {
 
     private ConfirmationType confirmationType;
@@ -18,5 +20,11 @@ public class ConfirmationConfig {
 
     public String getConfirmationMessage() {
         return confirmationMessage;
+    }
+
+    public void setPlaceholders(Map<String, String> placeholders) {
+        for (Map.Entry<String, String> entry : placeholders.entrySet()) {
+            confirmationMessage = confirmationMessage.replace(entry.getKey(), entry.getValue());
+        }
     }
 }
--- StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/mode/handler/CustomModuleExecutor.java
+++ StaffPlusCore/src/main/java/net/shortninja/staffplus/staff/mode/handler/CustomModuleExecutor.java
@@ -2,9 +2,11 @@ package net.shortninja.staffplus.staff.mode.handler;
 
 import org.bukkit.entity.Player;
 
+import java.util.Map;
+
 @FunctionalInterface
 public interface CustomModuleExecutor {
 
-    void execute(Player player);
+    void execute(Player player, Map<String, String> placeholders);
 
 }
