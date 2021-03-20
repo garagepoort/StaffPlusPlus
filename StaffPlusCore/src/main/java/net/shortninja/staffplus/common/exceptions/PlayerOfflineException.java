124b6dc7d00907e615cd203531d7cd5fc0542c4c
David Maes <david.s.maes@gmail.com>
2021-03-20 22:19:46 +0100

    Remove discord and trello

--- StaffPlusCore/src/main/java/net/shortninja/staffplus/common/exceptions/PlayerOfflineException.java
+++
@@ -1,9 +0,0 @@
-package net.shortninja.staffplus.common.exceptions;
-
-import net.shortninja.staffplus.application.IocContainer;
-
-public class PlayerOfflineException extends BusinessException {
-    public PlayerOfflineException() {
-        super(IocContainer.getMessages().playerOffline);
-    }
-}
