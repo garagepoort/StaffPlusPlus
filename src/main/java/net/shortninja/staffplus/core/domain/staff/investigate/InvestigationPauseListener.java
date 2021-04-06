e69c6f4f907f6cd5ca75e97816cfcbaada02d9b5
David Maes <david.s.maes@gmail.com>
2021-04-06 02:15:08 +0200

    Fix async namechange

--- src/main/java/net/shortninja/staffplus/core/domain/staff/investigate/InvestigationPauseListener.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/investigate/InvestigationPauseListener.java
@@ -2,23 +2,28 @@ package net.shortninja.staffplus.core.domain.staff.investigate;
 
 import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocListener;
+import net.shortninja.staffplus.core.common.config.Options;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-@IocBean(conditionalOnProperty = "investigations-module.automatic-pause=true")
+@IocBean(conditionalOnProperty = "investigations-module.enabled=true")
 @IocListener
 public class InvestigationPauseListener implements Listener {
 
     private final InvestigationService investigationService;
+    private final Options options;
 
-    public InvestigationPauseListener(InvestigationService investigationService) {
+    public InvestigationPauseListener(InvestigationService investigationService, Options options) {
         this.investigationService = investigationService;
+        this.options = options;
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onQuit(PlayerQuitEvent event) {
-        investigationService.pauseInvestigationsForInvestigated(event.getPlayer());
+        if(options.investigationConfiguration.isAutomaticPause()) {
+            investigationService.pauseInvestigationsForInvestigated(event.getPlayer());
+        }
     }
 }
