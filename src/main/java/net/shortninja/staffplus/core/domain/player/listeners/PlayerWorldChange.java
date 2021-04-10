abe91d4763c833e5c04472a3f1fcfe0bdafdf9d2
David Maes <david.s.maes@gmail.com>
2021-04-10 16:14:26 +0200

    Support staff mode world restriction

--- src/main/java/net/shortninja/staffplus/core/domain/player/listeners/PlayerWorldChange.java
+++ src/main/java/net/shortninja/staffplus/core/domain/player/listeners/PlayerWorldChange.java
@@ -1,12 +1,12 @@
 package net.shortninja.staffplus.core.domain.player.listeners;
 
 import be.garagepoort.mcioc.IocBean;
-import net.shortninja.staffplus.core.StaffPlus;
+import be.garagepoort.mcioc.IocListener;
 import net.shortninja.staffplus.core.domain.staff.mode.StaffModeService;
 import net.shortninja.staffplus.core.domain.staff.tracing.TraceService;
 import net.shortninja.staffplus.core.session.PlayerSession;
 import net.shortninja.staffplus.core.session.SessionManagerImpl;
-import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -15,6 +15,7 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
 import static net.shortninja.staffplus.core.domain.staff.tracing.TraceType.WORLD_CHANGE;
 
 @IocBean
+@IocListener
 public class PlayerWorldChange implements Listener {
     private final StaffModeService staffModeService;
     private final TraceService traceService;
@@ -24,16 +25,16 @@ public class PlayerWorldChange implements Listener {
         this.staffModeService = staffModeService;
         this.traceService = traceService;
         this.sessionManager = sessionManager;
-        Bukkit.getPluginManager().registerEvents(this, StaffPlus.get());
     }
-
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onWorldChange(PlayerChangedWorldEvent event) {
         PlayerSession session = sessionManager.get(event.getPlayer().getUniqueId());
-        if (session.isInStaffMode() && session.getModeConfiguration().get().isDisableOnWorldChange()) {
+        World currentWorld = event.getPlayer().getWorld();
+
+        if (session.isInStaffMode() && (session.getModeConfiguration().get().isDisableOnWorldChange() || !session.getModeConfiguration().get().isModeValidInWorld(currentWorld))) {
             staffModeService.removeMode(event.getPlayer());
         }
-        traceService.sendTraceMessage(WORLD_CHANGE, event.getPlayer().getUniqueId(), String.format("World changed from [%s] to [%s]", event.getFrom().getName(), event.getPlayer().getWorld().getName()));
+        traceService.sendTraceMessage(WORLD_CHANGE, event.getPlayer().getUniqueId(), String.format("World changed from [%s] to [%s]", event.getFrom().getName(), currentWorld.getName()));
     }
 }
