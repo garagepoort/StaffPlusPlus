4d5b1a0193c382d6d27781c5f8e0bffb9dedff2e
David Maes <david.s.maes@gmail.com>
2021-04-11 02:56:01 +0200

    Support keeping specific staffmode across bungeee

---
+++ src/main/java/net/shortninja/staffplus/core/application/database/migrations/common/V49_AlterSessionTableAddStaffModeNameColumnMigration.java
@@ -0,0 +1,16 @@
+package net.shortninja.staffplus.core.application.database.migrations.common;
+
+import be.garagepoort.mcsqlmigrations.Migration;
+
+public class V47_AlterInvestigationEvidenceTableAddDescriptionColumnMigration implements Migration {
+
+    @Override
+    public String getStatement() {
+        return "ALTER TABLE sp_investigation_evidence ADD COLUMN description TEXT NOT NULL DEFAULT '';";
+    }
+
+    @Override
+    public int getVersion() {
+        return 47;
+    }
+}
