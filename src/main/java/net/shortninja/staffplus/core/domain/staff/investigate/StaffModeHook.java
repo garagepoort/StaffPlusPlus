5e817622836a8837abfd824f067fdf35133e9fcf
David Maes <david.s.maes@gmail.com>
2021-04-13 23:28:30 +0200

    Support switching staff mode

--- src/main/java/net/shortninja/staffplus/core/domain/staff/investigate/StaffModeHook.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/investigate/StaffModeHook.java
@@ -6,8 +6,12 @@ import net.shortninja.staffplus.core.common.config.Options;
 import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplus.core.domain.staff.mode.StaffModeService;
 import net.shortninja.staffplusplus.investigate.InvestigationStartedEvent;
+import net.shortninja.staffplusplus.staffmode.ExitStaffModeEvent;
+import net.shortninja.staffplusplus.staffmode.SwitchStaffModeEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Optional;
 
 @IocBean
 @IocListener
@@ -16,11 +20,13 @@ public class StaffModeHook implements Listener {
     private final StaffModeService staffModeService;
     private final PlayerManager playerManager;
     private final Options options;
+    private final InvestigationService investigationService;
 
-    public StaffModeHook(StaffModeService staffModeService, PlayerManager playerManager, Options options) {
+    public StaffModeHook(StaffModeService staffModeService, PlayerManager playerManager, Options options, InvestigationService investigationService) {
         this.staffModeService = staffModeService;
         this.playerManager = playerManager;
         this.options = options;
+        this.investigationService = investigationService;
     }
 
     @EventHandler
@@ -34,6 +40,20 @@ public class StaffModeHook implements Listener {
                         staffModeService.turnStaffModeOn(p.getPlayer());
                     }
                 });
+        }
+    }
+
+    @EventHandler
+    public void handleExitStaffMode(ExitStaffModeEvent exitStaffModeEvent) {
+        if (options.investigationConfiguration.isEnforceStaffMode()) {
+            playerManager.getOnlinePlayer(exitStaffModeEvent.getPlayerUuid()).ifPresent(p -> investigationService.tryPausingInvestigation(p.getPlayer()));
+        }
+    }
+    @EventHandler
+    public void handleExitStaffMode(SwitchStaffModeEvent switchStaffModeEvent) {
+        Optional<String> staffModeConfig = options.investigationConfiguration.getStaffMode();
+        if (options.investigationConfiguration.isEnforceStaffMode() && staffModeConfig.isPresent() && !staffModeConfig.get().equalsIgnoreCase(switchStaffModeEvent.getToMode())) {
+            playerManager.getOnlinePlayer(switchStaffModeEvent.getPlayerUuid()).ifPresent(p -> investigationService.tryPausingInvestigation(p.getPlayer()));
         }
     }
 }
