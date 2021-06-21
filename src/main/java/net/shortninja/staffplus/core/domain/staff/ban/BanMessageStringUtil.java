32ed34156f2fac215edd1ae6d9ad25b047e1392f
David Maes <david.s.maes@gmail.com>
2021-06-21 11:51:59 +0200

    Fix duration tempban

--- src/main/java/net/shortninja/staffplus/core/domain/staff/ban/BanMessageStringUtil.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/ban/BanMessageStringUtil.java
@@ -1,15 +1,15 @@
 package net.shortninja.staffplus.core.domain.staff.ban;
 
-import net.shortninja.staffplus.core.common.JavaUtils;
+import net.shortninja.staffplusplus.ban.IBan;
 
 public class BanMessageStringUtil {
 
-    public static String replaceBanPlaceholders(String message, String target, String issuerName, String reason, Long endTimestamp) {
+    public static String replaceBanPlaceholders(String message, IBan ban) {
         String result = message;
-        if (target != null) result = result.replace("%target%", target);
-        if (issuerName != null) result = result.replace("%issuer%", issuerName);
-        if (reason != null) result = result.replace("%reason%", reason);
-        if (endTimestamp != null) result = result.replace("%duration%", JavaUtils.toHumanReadableDuration(JavaUtils.getDuration(endTimestamp)));
+        if (ban.getTargetName() != null) result = result.replace("%target%", ban.getTargetName());
+        if (ban.getIssuerName() != null) result = result.replace("%issuer%", ban.getIssuerName());
+        if (ban.getReason() != null) result = result.replace("%reason%", ban.getReason());
+        if (ban.getEndTimestamp() != null) result = result.replace("%duration%", ban.getHumanReadableDuration());
         return result;
     }
 }
