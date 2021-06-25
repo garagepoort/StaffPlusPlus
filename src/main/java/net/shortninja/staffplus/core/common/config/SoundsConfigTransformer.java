edc148546cee242fcc3c4078290f9bfc1c1c95e8
David Maes <david.s.maes@gmail.com>
2021-06-25 22:32:12 +0200

    Add warning notification

---
+++ src/main/java/net/shortninja/staffplus/core/common/config/SoundsConfigTransformer.java
@@ -0,0 +1,36 @@
+package net.shortninja.staffplus.core.common.config;
+
+import be.garagepoort.mcioc.configuration.IConfigTransformer;
+import net.shortninja.staffplus.core.StaffPlus;
+import net.shortninja.staffplus.core.common.JavaUtils;
+import net.shortninja.staffplus.core.common.Sounds;
+
+public class SoundsConfigTransformer implements IConfigTransformer<Sounds, String> {
+    @Override
+    public Sounds mapConfig(String configValue) {
+        return stringToSound(sanitize(configValue));
+    }
+
+    private Sounds stringToSound(String string) {
+        Sounds sound = Sounds.ORB_PICKUP;
+        boolean isValid = JavaUtils.isValidEnum(Sounds.class, string);
+        if (string.equalsIgnoreCase("NONE")) {
+            return null;
+        }
+        if (!isValid) {
+            StaffPlus.get().getLogger().warning("Invalid sound name '" + string + "'!");
+            return null;
+        } else sound = Sounds.valueOf(string);
+
+        return sound;
+    }
+
+
+    private String sanitize(String string) {
+        if (string.contains(":")) {
+            string = string.replace(string.substring(string.lastIndexOf(':')), "");
+        }
+
+        return string.toUpperCase();
+    }
+}
