353965c2e5e3da5c9e1e4d40b6d7b99f6d4362b2
David Maes <david.s.maes@gmail.com>
2021-07-07 00:25:02 +0200

    Add ipbancheck and ipbans command

--- src/main/java/net/shortninja/staffplus/core/domain/staff/ban/ipbans/gui/cmd/IpBanCheckCmd.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/ban/ipbans/gui/cmd/IpBanCheckCmd.java
@@ -8,62 +8,56 @@ import net.shortninja.staffplus.core.common.cmd.AbstractCmd;
 import net.shortninja.staffplus.core.common.cmd.CommandService;
 import net.shortninja.staffplus.core.common.cmd.PlayerRetrievalStrategy;
 import net.shortninja.staffplus.core.common.cmd.SppCommand;
-import net.shortninja.staffplus.core.common.exceptions.BusinessException;
 import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplus.core.domain.player.ip.database.PlayerIpRepository;
+import net.shortninja.staffplus.core.domain.staff.ban.ipbans.IpBan;
 import net.shortninja.staffplus.core.domain.staff.ban.ipbans.IpBanConfiguration;
 import net.shortninja.staffplus.core.domain.staff.ban.ipbans.IpBanService;
 import net.shortninja.staffplusplus.session.SppPlayer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
-import static net.shortninja.staffplus.core.common.JavaUtils.isValidCidrOrIp;
 import static net.shortninja.staffplus.core.common.utils.BukkitUtils.getIpFromPlayer;
 
 @IocBean(conditionalOnProperty = "ban-module.ipban.enabled=true")
 @IocMultiProvider(SppCommand.class)
-public class IpBanCmd extends AbstractCmd {
+public class IpBanCheckCmd extends AbstractCmd {
 
     private final IpBanService banService;
     private final PlayerManager playerManager;
     private final PlayerIpRepository playerIpRepository;
-    private final IpBanCmdUtil ipBanCmdUtil;
 
-    public IpBanCmd(Messages messages, Options options, IpBanConfiguration banConfiguration, IpBanService banService, CommandService commandService, PlayerManager playerManager, PlayerIpRepository playerIpRepository, IpBanCmdUtil ipBanCmdUtil) {
-        super(banConfiguration.commandIpBan, messages, options, commandService);
+    public IpBanCheckCmd(Messages messages, Options options, IpBanConfiguration banConfiguration, IpBanService banService, CommandService commandService, PlayerManager playerManager, PlayerIpRepository playerIpRepository) {
+        super(banConfiguration.commandIpBanCheck, messages, options, commandService);
         this.banService = banService;
         this.playerManager = playerManager;
         this.playerIpRepository = playerIpRepository;
-        this.ipBanCmdUtil = ipBanCmdUtil;
-        setPermission(banConfiguration.permissionIpBan);
-        setDescription("Permanent ban an ip-address");
-        setUsage("[player/ip-address]");
+        setPermission(banConfiguration.permissionIpBanCheck);
+        setDescription("Check if a player is ip banned");
+        setUsage("[player]");
     }
 
 
     @Override
     protected boolean executeCmd(CommandSender sender, String alias, String[] args, SppPlayer player, Map<String, String> optionalParameters) {
-        String ipArg = args[0];
-        if (!isValidCidrOrIp(ipArg)) {
-            SppPlayer sppPlayer = playerManager.getOnOrOfflinePlayer(ipArg).orElseThrow(() -> new BusinessException("No player found with or Ip address given is invalid"));
-            ipArg = getIpAddress(sppPlayer);
-        }
-        String ipAddress = ipArg;
-        String template = ipBanCmdUtil.retrieveTemplate(sender, optionalParameters);
-        if (sender instanceof Player) {
-            ipBanCmdUtil.sendBanChoiceMessage((Player) sender, ipAddress, p -> banService.banIp(sender, ipAddress, template, optionalParameters.containsKey("-silent")));
+        Optional<String> ipAddress = getIpAddress(player);
+        List<IpBan> matchingIpBans = ipAddress.isPresent() ? banService.findMatchingIpBans(ipAddress.get()) : Collections.emptyList();
+        if (matchingIpBans.isEmpty()) {
+            messages.send(sender, "&6This player is &2not ip banned.", messages.prefixBans);
         } else {
-            banService.banIp(sender, ipAddress, template, optionalParameters.containsKey("-silent"));
+            messages.send(sender, "&6This player is &cip banned &6by following rules: ", messages.prefixBans);
+            messages.send(sender, messages.LONG_LINE, messages.prefixBans);
+            for (int i = 0, matchingIpBansSize = matchingIpBans.size(); i < matchingIpBansSize; i++) {
+                IpBan matchingIpBan = matchingIpBans.get(i);
+                messages.send(sender, "&c" + (i + 1) + ". &7" + matchingIpBan.getIp(), messages.prefixBans);
+            }
         }
         return true;
-    }
-
-    @Override
-    protected List<String> getOptionalParameters() {
-        return Arrays.asList("-silent", "-template");
     }
 
     @Override
@@ -73,12 +67,12 @@ public class IpBanCmd extends AbstractCmd {
 
     @Override
     protected PlayerRetrievalStrategy getPlayerRetrievalStrategy() {
-        return PlayerRetrievalStrategy.NONE;
+        return PlayerRetrievalStrategy.BOTH;
     }
 
     @Override
     protected Optional<String> getPlayerName(CommandSender sender, String[] args) {
-        return Optional.empty();
+        return Optional.ofNullable(args[0]);
     }
 
     @Override
@@ -94,7 +88,7 @@ public class IpBanCmd extends AbstractCmd {
         return Collections.emptyList();
     }
 
-    private String getIpAddress(SppPlayer sppPlayer) {
-        return sppPlayer.isOnline() ? getIpFromPlayer(sppPlayer.getPlayer()) : playerIpRepository.getLastIp(sppPlayer.getId()).orElseThrow(() -> new BusinessException("This player's Ip address is unknown."));
+    private Optional<String> getIpAddress(SppPlayer sppPlayer) {
+        return sppPlayer.isOnline() ? Optional.of(getIpFromPlayer(sppPlayer.getPlayer())) : playerIpRepository.getLastIp(sppPlayer.getId());
     }
 }
