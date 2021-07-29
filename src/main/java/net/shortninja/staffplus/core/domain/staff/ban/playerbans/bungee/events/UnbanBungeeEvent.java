a6258de05c71c611a2176ab8aa74e7231b8a2029
David Maes <david.s.maes@gmail.com>
2021-07-29 16:54:13 +0200

    Ban messages across bungee

---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/ban/playerbans/bungee/events/UnbanBungeeEvent.java
@@ -0,0 +1,29 @@
+package net.shortninja.staffplus.core.domain.staff.ban.playerbans.bungee.events;
+
+import net.shortninja.staffplus.core.domain.staff.ban.playerbans.bungee.dto.BanBungeeDto;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class UnbanBungeeEvent extends Event {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+    private final BanBungeeDto ban;
+
+    public UnbanBungeeEvent(BanBungeeDto ban) {
+        this.ban = ban;
+    }
+
+    public BanBungeeDto getBan() {
+        return ban;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+}
