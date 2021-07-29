a321396ca262794a3adf90edcb88dab7192d259d
David Maes <david.s.maes@gmail.com>
2021-07-29 21:43:35 +0200

    Ipban messages across bungee

--- src/main/java/net/shortninja/staffplus/core/domain/staff/ban/ipbans/IpBanMessageStringUtil.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/ban/ipbans/IpBanMessageStringUtil.java
@@ -1,5 +1,6 @@
 package net.shortninja.staffplus.core.domain.staff.ban.ipbans;
 
+import net.shortninja.staffplus.core.domain.staff.ban.ipbans.bungee.dto.IpBanBungeeDto;
 import net.shortninja.staffplusplus.ban.IIpBan;
 
 public class IpBanMessageStringUtil {
@@ -9,6 +10,14 @@ public class IpBanMessageStringUtil {
         if (ban.getIssuerName() != null) result = result.replace("%issuer%", ban.getIssuerName());
         if (ban.getIp() != null) result = result.replace("%ip%", ban.getIp());
         if (ban.getEndTimestamp().isPresent()) result = result.replace("%duration%", ban.getHumanReadableDuration());
+        return result;
+    }
+
+    public static String replaceBanPlaceholders(String message, IpBanBungeeDto ban) {
+        String result = message;
+        if (ban.getIssuerName() != null) result = result.replace("%issuer%", ban.getIssuerName());
+        if (ban.getIp() != null) result = result.replace("%ip%", ban.getIp());
+        if (ban.getEndTimestamp() != null) result = result.replace("%duration%", ban.getHumanReadableDuration());
         return result;
     }
 }
---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/ban/ipbans/bungee/IpBanBungeeNotifier.java
@@ -0,0 +1,39 @@
+package net.shortninja.staffplus.core.domain.staff.ban.ipbans.bungee;
+
+import be.garagepoort.mcioc.IocBean;
+import be.garagepoort.mcioc.IocListener;
+import net.shortninja.staffplus.core.common.bungee.BungeeClient;
+import net.shortninja.staffplus.core.domain.staff.ban.ipbans.bungee.dto.IpBanBungeeDto;
+import net.shortninja.staffplusplus.ban.IpBanEvent;
+import net.shortninja.staffplusplus.ban.IpUnbanEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import static net.shortninja.staffplus.core.common.Constants.BUNGEE_IP_BANNED_CHANNEL;
+import static net.shortninja.staffplus.core.common.Constants.BUNGEE_IP_UNBANNED_CHANNEL;
+
+@IocBean(conditionalOnProperty = "server-sync-module.ban-sync=true")
+@IocListener
+public class IpBanBungeeNotifier implements Listener {
+
+    private final BungeeClient bungeeClient;
+
+    public IpBanBungeeNotifier(BungeeClient bungeeClient) {
+        this.bungeeClient = bungeeClient;
+    }
+
+    @EventHandler
+    public void onBan(IpBanEvent event) {
+        Player player = Bukkit.getOnlinePlayers().iterator().next();
+        bungeeClient.sendMessage(player, BUNGEE_IP_BANNED_CHANNEL, new IpBanBungeeDto(event.getBan(), event.getKickTemplate().orElse(null)));
+    }
+
+    @EventHandler
+    public void onUnban(IpUnbanEvent event) {
+        Player player = Bukkit.getOnlinePlayers().iterator().next();
+        bungeeClient.sendMessage(player, BUNGEE_IP_UNBANNED_CHANNEL, new IpBanBungeeDto(event.getBan()));
+    }
+
+}
