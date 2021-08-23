85b7229d81f7df6acb973a69bf6df3e312159c9e
David Maes <david.s.maes@gmail.com>
2021-08-23 13:53:48 +0200

    Add blocked commands for muted players

--- src/main/java/net/shortninja/staffplus/core/domain/staff/mute/config/MuteConfiguration.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/mute/config/MuteConfiguration.java
@@ -3,11 +3,15 @@ package net.shortninja.staffplus.core.domain.staff.mute.config;
 import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.configuration.ConfigProperty;
 
+import java.util.List;
+
 @IocBean
 public class MuteConfiguration {
 
     @ConfigProperty("mute-module.enabled")
     public boolean muteEnabled;
+    @ConfigProperty("mute-module.blocked-commands")
+    public List<String> blockedCommands;
 
     @ConfigProperty("permissions:tempmute")
     public String permissionTempmutePlayer;
