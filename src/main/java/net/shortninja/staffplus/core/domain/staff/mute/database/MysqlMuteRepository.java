4e0be76a27906e45b6587ae3aee91ae9380e0fc8
David Maes <david.s.maes@gmail.com>
2021-08-24 03:50:25 +0200

    Add softmute

--- src/main/java/net/shortninja/staffplus/core/domain/staff/mute/database/MysqlMuteRepository.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/mute/database/MysqlMuteRepository.java
@@ -24,8 +24,8 @@ public class MysqlMuteRepository extends AbstractSqlMuteRepository {
     @Override
     public int addMute(Mute mute) {
         try (Connection sql = getConnection();
-             PreparedStatement insert = sql.prepareStatement("INSERT INTO sp_muted_players(reason, player_uuid, player_name, issuer_uuid, issuer_name, end_timestamp, creation_timestamp, server_name) " +
-                 "VALUES(?, ?, ?, ?, ?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement insert = sql.prepareStatement("INSERT INTO sp_muted_players(reason, player_uuid, player_name, issuer_uuid, issuer_name, end_timestamp, creation_timestamp, server_name, soft_mute) " +
+                 "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS)) {
             insert.setString(1, mute.getReason());
             insert.setString(2, mute.getTargetUuid().toString());
             insert.setString(3, mute.getTargetName());
@@ -38,6 +38,7 @@ public class MysqlMuteRepository extends AbstractSqlMuteRepository {
             }
             insert.setLong(7, mute.getCreationTimestamp());
             insert.setString(8, options.serverName);
+            insert.setBoolean(9, mute.isSoftMute());
             insert.executeUpdate();
 
             ResultSet generatedKeys = insert.getGeneratedKeys();
