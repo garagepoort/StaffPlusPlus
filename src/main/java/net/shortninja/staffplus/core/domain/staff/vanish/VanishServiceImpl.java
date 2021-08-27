42a396deb7a912fcff0de20b4fd0fc7ba28b7e07
David Maes <david.s.maes@gmail.com>
2021-08-27 22:36:49 +0200

    Fix vanish action bar task

--- src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/VanishServiceImpl.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/VanishServiceImpl.java
@@ -1,16 +1,11 @@
 package net.shortninja.staffplus.core.domain.staff.vanish;
 
 import be.garagepoort.mcioc.IocBean;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.shortninja.staffplus.core.StaffPlus;
-import net.shortninja.staffplus.core.application.config.Messages;
 import net.shortninja.staffplus.core.domain.player.settings.PlayerSettings;
 import net.shortninja.staffplus.core.domain.player.settings.PlayerSettingsRepository;
 import net.shortninja.staffplusplus.vanish.VanishOffEvent;
 import net.shortninja.staffplusplus.vanish.VanishOnEvent;
 import net.shortninja.staffplusplus.vanish.VanishType;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import static net.shortninja.staffplus.core.common.utils.BukkitUtils.sendEvent;
@@ -21,18 +16,10 @@ public class VanishServiceImpl {
     private final PlayerSettingsRepository playerSettingsRepository;
     private final VanishConfiguration vanishConfiguration;
 
-    public VanishServiceImpl(Messages messages,
-                             PlayerSettingsRepository playerSettingsRepository,
+    public VanishServiceImpl(PlayerSettingsRepository playerSettingsRepository,
                              VanishConfiguration vanishConfiguration) {
         this.playerSettingsRepository = playerSettingsRepository;
         this.vanishConfiguration = vanishConfiguration;
-        if (this.vanishConfiguration.vanishMessageEnabled) {
-            Bukkit.getScheduler().runTaskTimerAsynchronously(StaffPlus.get(), () -> {
-                Bukkit.getOnlinePlayers().stream()
-                    .filter(p -> playerSettingsRepository.get(p).isVanished())
-                    .forEach(p -> p.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(messages.colorize(messages.vanishEnabled))));
-            }, 20L, 20L);
-        }
     }
 
     public void addVanish(Player player, VanishType vanishType) {
