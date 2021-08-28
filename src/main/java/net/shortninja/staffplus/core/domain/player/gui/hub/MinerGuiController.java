2c97a7e871aaccfdd1c0d667b1469df0fa6c1b7c
David Maes <david.s.maes@gmail.com>
2021-08-29 01:58:15 +0200

    Improve staff and miners gui

--- src/main/java/net/shortninja/staffplus/core/domain/player/gui/hub/MinerGuiController.java
+++ src/main/java/net/shortninja/staffplus/core/domain/player/gui/hub/MinerGuiController.java
@@ -1,39 +1,53 @@
 package net.shortninja.staffplus.core.domain.player.gui.hub;
 
 import be.garagepoort.mcioc.IocBean;
-import be.garagepoort.mcioc.gui.CurrentAction;
 import be.garagepoort.mcioc.gui.GuiAction;
 import be.garagepoort.mcioc.gui.GuiController;
 import be.garagepoort.mcioc.gui.GuiParam;
-import be.garagepoort.mcioc.gui.TubingGui;
+import be.garagepoort.mcioc.gui.templates.GuiTemplate;
 import net.shortninja.staffplus.core.application.config.Messages;
+import net.shortninja.staffplus.core.application.config.Options;
+import net.shortninja.staffplus.core.common.JavaUtils;
 import net.shortninja.staffplus.core.domain.player.PlayerManager;
-import net.shortninja.staffplus.core.domain.player.gui.hub.views.MinerViewBuilder;
 import net.shortninja.staffplusplus.session.SppPlayer;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static be.garagepoort.mcioc.gui.templates.GuiTemplate.template;
 
 @IocBean
 @GuiController
 public class MinerGuiController {
 
-    private final MinerViewBuilder minerViewBuilder;
     private final PlayerManager playerManager;
     private final Messages messages;
+    private final Options options;
 
-    public MinerGuiController(MinerViewBuilder minerViewBuilder, PlayerManager playerManager, Messages messages) {
-        this.minerViewBuilder = minerViewBuilder;
+    public MinerGuiController(PlayerManager playerManager, Messages messages, Options options) {
         this.playerManager = playerManager;
         this.messages = messages;
+        this.options = options;
     }
 
     @GuiAction("miners/view")
-    public TubingGui getMinersView(@GuiParam(value = "page", defaultValue = "0") int page,
-                                   @CurrentAction String currentAction,
-                                   @GuiParam("backAction") String backAction) {
-        return minerViewBuilder.buildGui(page, currentAction, backAction);
+    public GuiTemplate getMinersView(@GuiParam(value = "page", defaultValue = "0") int page) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("players", getPlayers(page));
+        return template("gui/player/miners-overview.ftl", params);
+    }
+
+    private List<SppPlayer> getPlayers(int page) {
+        List<SppPlayer> onlinePlayers = playerManager.getOnlineSppPlayers().stream()
+            .filter(p -> p.getPlayer().getLocation().getBlockY() < options.staffItemsConfiguration.getGuiModeConfiguration().modeGuiMinerLevel)
+            .collect(Collectors.toList());
+
+        return JavaUtils.getPageOfList(new ArrayList<>(onlinePlayers), page, 45);
     }
 
     @GuiAction("miners/teleport")
