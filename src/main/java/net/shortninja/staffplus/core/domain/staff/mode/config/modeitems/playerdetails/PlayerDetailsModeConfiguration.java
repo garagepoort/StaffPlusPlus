64912f25ccc7144aebe3bf2225b8b1e23412eabe
David Maes <david.s.maes@gmail.com>
2021-08-28 13:45:20 +0200

    Add player detail item

---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/mode/config/modeitems/playerdetails/PlayerDetailsModeConfiguration.java
@@ -0,0 +1,10 @@
+package net.shortninja.staffplus.core.domain.staff.mode.config.modeitems.playerdetails;
+
+import net.shortninja.staffplus.core.domain.staff.mode.config.ModeItemConfiguration;
+
+public class PlayerDetailsModeConfiguration extends ModeItemConfiguration {
+
+    public PlayerDetailsModeConfiguration(String identifier) {
+        super(identifier);
+    }
+}
