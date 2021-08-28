ee1fab999a54c7317d3f4d77e373578146847017
David Maes <david.s.maes@gmail.com>
2021-08-28 23:05:15 +0200

    Fix player settings repository

--- src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/listeners/MobsAttackingVanishedListener.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/listeners/MobsAttackingVanishedListener.java
@@ -2,11 +2,9 @@ package net.shortninja.staffplus.core.domain.staff.vanish.listeners;
 
 import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocListener;
+import net.shortninja.staffplus.core.application.session.OnlinePlayerSession;
 import net.shortninja.staffplus.core.application.session.OnlineSessionsManager;
-import net.shortninja.staffplus.core.domain.player.settings.PlayerSettings;
-import net.shortninja.staffplus.core.domain.player.settings.PlayerSettingsRepository;
 import net.shortninja.staffplusplus.vanish.VanishType;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -17,18 +15,16 @@ import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 @IocListener
 public class MobsAttackingVanishedListener implements Listener {
     private final OnlineSessionsManager sessionManager;
-    private final PlayerSettingsRepository playerSettingsRepository;
 
-    public MobsAttackingVanishedListener(OnlineSessionsManager sessionManager, PlayerSettingsRepository playerSettingsRepository) {
+    public MobsAttackingVanishedListener(OnlineSessionsManager sessionManager) {
         this.sessionManager = sessionManager;
-        this.playerSettingsRepository = playerSettingsRepository;
     }
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onTarget(EntityTargetLivingEntityEvent event) {
         if (event.getTarget() instanceof Player && sessionManager.has(event.getTarget().getUniqueId())) {
-            PlayerSettings playerSettings = playerSettingsRepository.get((OfflinePlayer) event.getTarget());
-            if (playerSettings.getVanishType() == VanishType.TOTAL || playerSettings.getVanishType() == VanishType.PLAYER) {
+            OnlinePlayerSession session = sessionManager.get((Player) event.getTarget());
+            if (session.getVanishType() == VanishType.TOTAL || session.getVanishType() == VanishType.PLAYER) {
                 event.setCancelled(true);
             }
         }
