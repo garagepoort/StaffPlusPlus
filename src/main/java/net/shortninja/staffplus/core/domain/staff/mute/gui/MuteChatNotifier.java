0721b8a86598b4b144fe243256a78498a8ba1823
David Maes <david.s.maes@gmail.com>
2021-09-19 21:49:54 +0200

    Added reduce/extend mute command

--- src/main/java/net/shortninja/staffplus/core/domain/staff/mute/gui/MuteChatNotifier.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/mute/gui/MuteChatNotifier.java
@@ -3,9 +3,13 @@ package net.shortninja.staffplus.core.domain.staff.mute.gui;
 import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocListener;
 import net.shortninja.staffplus.core.application.config.Messages;
+import net.shortninja.staffplus.core.common.JavaUtils;
+import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplus.core.domain.staff.mute.config.MuteConfiguration;
 import net.shortninja.staffplusplus.mute.IMute;
 import net.shortninja.staffplusplus.mute.MuteEvent;
+import net.shortninja.staffplusplus.mute.MuteExtensionEvent;
+import net.shortninja.staffplusplus.mute.MuteReductionEvent;
 import net.shortninja.staffplusplus.mute.UnmuteEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -19,10 +23,12 @@ public class MuteChatNotifier implements Listener {
 
     private final Messages messages;
     private final MuteConfiguration muteConfiguration;
+    private final PlayerManager playerManager;
 
-    public MuteChatNotifier(Messages messages, MuteConfiguration muteConfiguration) {
+    public MuteChatNotifier(Messages messages, MuteConfiguration muteConfiguration, PlayerManager playerManager) {
         this.messages = messages;
         this.muteConfiguration = muteConfiguration;
+        this.playerManager = playerManager;
     }
 
     @EventHandler
@@ -43,6 +49,21 @@ public class MuteChatNotifier implements Listener {
         String unmuteMessage = replaceMutePlaceholders(messages.unmuted, mute);
         messages.sendGroupMessage(unmuteMessage, muteConfiguration.staffNotificationPermission, messages.prefixGeneral);
     }
+
+    @EventHandler
+    public void notifyMuteExtension(MuteExtensionEvent event) {
+        IMute mute = event.getMute();
+        String message = replaceMutePlaceholders(messages.muteExtended, mute).replace("%extensionDuration%", JavaUtils.toHumanReadableDuration(event.getExtensionDuration()));
+        messages.send(event.getExecutor(), message, messages.prefixGeneral);
+    }
+
+    @EventHandler
+    public void notifyMuteReduction(MuteReductionEvent event) {
+        IMute mute = event.getMute();
+        String message = replaceMutePlaceholders(messages.muteReduced, mute).replace("%reductionDuration%", JavaUtils.toHumanReadableDuration(event.getReductionDuration()));
+        messages.send(event.getExecutor(), message, messages.prefixGeneral);
+    }
+
 
     public void notifyPlayerMuted(IMute mute, Player player) {
         if (!mute.isSoftMute()) {
