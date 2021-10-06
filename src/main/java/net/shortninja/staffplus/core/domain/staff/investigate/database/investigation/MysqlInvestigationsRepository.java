488fd2ac217c8a4e9886a15960e4a3f871ce009e
David Maes <david.s.maes@gmail.com>
2021-10-06 20:25:48 +0200

    Fix missing key for spanish translation

--- src/main/java/net/shortninja/staffplus/core/domain/staff/investigate/database/investigation/MysqlInvestigationsRepository.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/investigate/database/investigation/MysqlInvestigationsRepository.java
@@ -24,17 +24,20 @@ public class MysqlInvestigationsRepository extends AbstractSqlInvestigationsRepo
     @Override
     public int addInvestigation(Investigation investigation) {
         try (Connection sql = getConnection();
-             PreparedStatement insert = sql.prepareStatement("INSERT INTO sp_investigations(investigator_uuid, investigated_uuid, status, creation_timestamp, server_name) " +
-                 "VALUES(?, ?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement insert = sql.prepareStatement("INSERT INTO sp_investigations(investigator_uuid, investigator_name, investigated_uuid, investigated_name, status, creation_timestamp, server_name) " +
+                 "VALUES(?, ?, ?, ?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS)) {
             insert.setString(1, investigation.getInvestigatorUuid().toString());
+            insert.setString(2, investigation.getInvestigatorName());
             if(investigation.getInvestigatedUuid().isPresent()) {
-                insert.setString(2, investigation.getInvestigatedUuid().get().toString());
+                insert.setString(3, investigation.getInvestigatedUuid().get().toString());
+                insert.setString(4, investigation.getInvestigatedName().get());
             }else {
-                insert.setNull(2, Types.VARCHAR);
+                insert.setNull(3, Types.VARCHAR);
+                insert.setNull(4, Types.VARCHAR);
             }
-            insert.setString(3, investigation.getStatus().name());
-            insert.setLong(4, investigation.getCreationTimestamp());
-            insert.setString(5, options.serverName);
+            insert.setString(5, investigation.getStatus().name());
+            insert.setLong(6, investigation.getCreationTimestamp());
+            insert.setString(7, options.serverName);
             insert.executeUpdate();
 
             ResultSet generatedKeys = insert.getGeneratedKeys();
