f2c4d2186814353ac0cd3b9639757e773a70eab3
David Maes <david.s.maes@gmail.com>
2021-11-03 22:40:27 +0100

    Refactor to use gui templates for warning detail

--- src/main/java/net/shortninja/staffplus/core/domain/staff/warn/warnings/gui/WarningsGuiController.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/warn/warnings/gui/WarningsGuiController.java
@@ -1,11 +1,8 @@
 package net.shortninja.staffplus.core.domain.staff.warn.warnings.gui;
 
 import be.garagepoort.mcioc.IocBean;
-import be.garagepoort.mcioc.gui.CurrentAction;
-import be.garagepoort.mcioc.gui.GuiAction;
-import be.garagepoort.mcioc.gui.GuiController;
-import be.garagepoort.mcioc.gui.GuiParam;
-import be.garagepoort.mcioc.gui.TubingGui;
+import be.garagepoort.mcioc.gui.*;
+import be.garagepoort.mcioc.gui.templates.GuiTemplate;
 import net.shortninja.staffplus.core.application.config.Messages;
 import net.shortninja.staffplus.core.application.config.Options;
 import net.shortninja.staffplus.core.application.session.OnlinePlayerSession;
@@ -13,6 +10,7 @@ import net.shortninja.staffplus.core.application.session.OnlineSessionsManager;
 import net.shortninja.staffplus.core.common.exceptions.BusinessException;
 import net.shortninja.staffplus.core.common.exceptions.PlayerNotFoundException;
 import net.shortninja.staffplus.core.common.utils.BukkitUtils;
+import net.shortninja.staffplus.core.domain.actions.ActionService;
 import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplus.core.domain.staff.examine.gui.SeverityLevelSelectViewBuilder;
 import net.shortninja.staffplus.core.domain.staff.warn.warnings.WarnService;
@@ -22,6 +20,13 @@ import net.shortninja.staffplusplus.session.SppPlayer;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static be.garagepoort.mcioc.gui.AsyncGui.async;
+import static be.garagepoort.mcioc.gui.templates.GuiTemplate.template;
+
 @IocBean
 @GuiController
 public class WarningsGuiController {
@@ -30,7 +35,6 @@ public class WarningsGuiController {
     private static final String NONE = "none";
 
     private final ManageWarningsViewBuilder manageWarningsViewBuilder;
-    private final ManageWarningViewBuilder manageWarningViewBuilder;
     private final MyWarningsViewBuilder myWarningsViewBuilder;
     private final ManageAppealedWarningsViewBuilder manageAppealedWarningsViewBuilder;
     private final SeverityLevelSelectViewBuilder severityLevelSelectViewBuilder;
@@ -40,15 +44,14 @@ public class WarningsGuiController {
     private final Options options;
     private final Messages messages;
     private final BukkitUtils bukkitUtils;
+    private final ActionService actionService;
 
     public WarningsGuiController(ManageWarningsViewBuilder manageWarningsViewBuilder,
-                                 ManageWarningViewBuilder manageWarningViewBuilder,
                                  MyWarningsViewBuilder myWarningsViewBuilder,
                                  ManageAppealedWarningsViewBuilder manageAppealedWarningsViewBuilder,
                                  SeverityLevelSelectViewBuilder severityLevelSelectViewBuilder, PlayerManager playerManager,
-                                 WarnService warnService, OnlineSessionsManager sessionManager, Options options, Messages messages, BukkitUtils bukkitUtils) {
+                                 WarnService warnService, OnlineSessionsManager sessionManager, Options options, Messages messages, BukkitUtils bukkitUtils, ActionService actionService) {
         this.manageWarningsViewBuilder = manageWarningsViewBuilder;
-        this.manageWarningViewBuilder = manageWarningViewBuilder;
         this.myWarningsViewBuilder = myWarningsViewBuilder;
         this.manageAppealedWarningsViewBuilder = manageAppealedWarningsViewBuilder;
         this.severityLevelSelectViewBuilder = severityLevelSelectViewBuilder;
@@ -58,6 +61,7 @@ public class WarningsGuiController {
         this.options = options;
         this.messages = messages;
         this.bukkitUtils = bukkitUtils;
+        this.actionService = actionService;
     }
 
     @GuiAction("manage-warnings/view/overview")
@@ -93,12 +97,18 @@ public class WarningsGuiController {
     }
 
     @GuiAction("manage-warnings/view/detail")
-    public TubingGui warningDetail(Player player,
-                                   @GuiParam("warningId") int warningId,
-                                   @CurrentAction String currentAction,
-                                   @GuiParam("backAction") String backAction) {
-        Warning warning = warnService.getWarning(warningId);
-        return manageWarningViewBuilder.buildGui(player, warning, currentAction, backAction);
+    public AsyncGui<GuiTemplate> warningDetail(@GuiParam("warningId") int warningId) {
+        return async(() -> {
+            Warning warning = warnService.getWarning(warningId);
+            Map<String, Object> params = new HashMap<>();
+            params.put("warning", warning);
+            params.put("rollbackCommands", actionService.getActions(warning).stream()
+                .filter(s -> s.isExecuted() && s.isRollbackable() && !s.isRollbacked())
+                .map(s -> s.getRollbackCommand().get().getCommand())
+                .collect(Collectors.toList()));
+
+            return template("gui/warnings/warning-detail.ftl", params);
+        });
     }
 
     @GuiAction("manage-warnings/delete")
---
+++ src/main/resources/gui/warnings/warning-detail.ftl
@@ -0,0 +1,84 @@
+<#import "warning-commons.ftl" as warningcommons/>
+<#import "/gui/commons/commons.ftl" as commons/>
+<#import "/gui/evidence/evidence-commons.ftl" as evidenceCommons/>
+<#assign GuiUtils=statics['net.shortninja.staffplus.core.common.gui.GuiUtils']>
+<#assign URLEncoder=statics['java.net.URLEncoder']>
+
+<#macro appealbutton action>
+    <GuiItem slot="31"
+             material="BOOK"
+             name="Add appeal"
+             onLeftClick="${action}">
+        <Lore>
+            <LoreLine>Click to expire this warning</LoreLine>
+        </Lore>
+    </GuiItem>
+</#macro>
+
+<TubingGui size="54">
+    <title>Warning for: ${warning.targetName}</title>
+
+    <GuiItem slot="13"
+             material="PLAYER_HEAD"
+             name="Warning">
+        <Lore>
+            <@warningcommons.warninglorelines warning=warning/>
+        </Lore>
+    </GuiItem>
+
+    <GuiItem slot="8"
+             permission="config|permissions:warnings.manage.delete"
+             material="REDSTONE_BLOCK"
+             name="Delete"
+             onLeftClick="manage-warnings/delete?warningId=${warning.id}">
+        <Lore>
+            <LoreLine>Click to delete this warning</LoreLine>
+            <#if rollbackCommands?size != 0>
+                <@commons.line />
+                <LoreLine>&6Rollback actions:</LoreLine>
+                <#list rollbackCommands as command>
+                    <LoreLine> - ${command}</LoreLine>
+                </#list>
+            </#if>
+        </Lore>
+    </GuiItem>
+
+    <#if !warning.expired && !warning.hasApprovedAppeal()>
+        <GuiItem slot="17"
+                 permission="config|permissions:warnings.manage.expire"
+                 material="ORANGE_STAINED_GLASS_PANE"
+                 name="Expire"
+                 onLeftClick="manage-warnings/expire?warningId=${warning.id}">
+            <Lore>
+                <LoreLine>Click to expire this warning</LoreLine>
+            </Lore>
+        </GuiItem>
+    </#if>
+
+    <#if $config.get("warnings-module.appeals.enabled")>
+        <#assign canAppeal=!warning.appeal.isPresent()
+        && ($permissions.has(player, $config.get("permissions:warnings.appeals.create-others"))
+        || ($permissions.has(player, $config.get("permissions:warnings.appeals.create")) && warning.targetUuid == player.uniqueId))/>
+
+        <#if canAppeal>
+            <#if $config.get("warnings-module.appeals.fixed-reason")>
+                <@appealbutton action="manage-warning-appeals/view/create/reason-select?warningId=${warning.id}" />
+            <#else>
+                <@appealbutton action="manage-warning-appeals/view/create/reason-chat?warningId=${warning.id}" />
+            </#if>
+        <#elseif warning.appeal.isPresent()>
+            <#if ($permissions.has(player, $config.get("permissions:warnings.appeals.approve"))
+                || $permission.has(player, $config.get("permissions:warnings.appeals.reject")))
+                && warning.appeal.get().status.name() == "OPEN">
+                <@warningcommons.appealinfoitem
+                appeal=warning.appeal.get()
+                action="manage-warning-appeals/view/detail?appealId=${warning.appeal.get().id}&backAction=${URLEncoder.encode(currentAction)}"/>
+            <#else>
+                <@warningcommons.appealinfoitem appeal=warning.appeal.get() action="$NOOP"/>
+            </#if>
+        </#if>
+    </#if>
+
+    <@evidenceCommons.evidenceButton slot=14 evidence=warning backAction=currentAction />
+    <@commons.backButton action=backAction/>
+</TubingGui>
