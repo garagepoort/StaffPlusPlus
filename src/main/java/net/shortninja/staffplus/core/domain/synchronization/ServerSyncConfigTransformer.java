6680adfb8eef6418f6ef5e293ad950debf7beacb
David Maes <david.s.maes@gmail.com>
2021-11-30 14:13:46 +0100

    Fix synced query

--- src/main/java/net/shortninja/staffplus/core/domain/synchronization/ServerSyncConfigTransformer.java
+++ src/main/java/net/shortninja/staffplus/core/domain/synchronization/ServerSyncConfigTransformer.java
@@ -2,10 +2,12 @@ package net.shortninja.staffplus.core.domain.synchronization;
 
 import be.garagepoort.mcioc.configuration.ConfigProperty;
 import be.garagepoort.mcioc.configuration.IConfigTransformer;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ServerSyncConfigTransformer implements IConfigTransformer<ServerSyncConfig, String> {
 
@@ -16,6 +18,8 @@ public class ServerSyncConfigTransformer implements IConfigTransformer<ServerSyn
     public ServerSyncConfig mapConfig(String s) {
         List<String> servers = new ArrayList<>(Arrays.asList(s.split("\\s*;\\s*")));
         servers.add(serverName);
-        return new ServerSyncConfig(servers);
+        return new ServerSyncConfig(servers.stream()
+            .filter(StringUtils::isNotBlank)
+            .collect(Collectors.toList()));
     }
 }
