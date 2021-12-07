3074e9384dee2aa79d5713f20644cb54cf64f5dc
David Maes <david.s.maes@gmail.com>
2021-12-07 01:47:01 +0100

    Fix IT test

--- src/test/java/net/shortninja/staffplus/core/RunPluginIT.java
+++ src/test/java/net/shortninja/staffplus/core/RunPluginIT.java
@@ -1,46 +1,52 @@
 package net.shortninja.staffplus.core;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @Testcontainers
 public class RunPluginIT {
 
-    //    @Container
-//    public GenericContainer container = new GenericContainer<>(new ImageFromDockerfile()
-//        .withFileFromClasspath("staffplusplus-core-1.18.0-SNAPSHOT.jar", "/docker/staffplusplus-core-1.18.0-SNAPSHOT.jar")
-//        .withFileFromClasspath("spigot-1.18.jar", "/docker/spigot-1.18.jar")
-//        .withFileFromClasspath("Dockerfile", "/docker/Dockerfile"))
-//        .waitingFor(Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(360)))
-//        .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("test")));
-    @Container
-    public GenericContainer container = new GenericContainer(
-        new ImageFromDockerfile()
-            .withFileFromClasspath("staffplusplus-core-1.18.0-SNAPSHOT.jar", "/docker/staffplusplus-core-1.18.0-SNAPSHOT.jar")
-            .withFileFromClasspath("spigot-1.18.jar", "/docker/spigot-1.18.jar")
-            .withFileFromClasspath("eula.txt", "/docker/eula.txt")
-            .withFileFromClasspath("server.properties", "/docker/server.properties")
-            .withDockerfileFromBuilder(builder ->
-                builder
-                    .from("openjdk:17")
-                    .copy("spigot-1.18.jar", ".")
-                    .copy("eula.txt", ".")
-                    .copy("server.properties", ".")
-                    .run("chmod", "777", "spigot-1.18.jar")
-                    .run("mkdir", "plugins")
-                    .copy("staffplusplus-core-1.18.0-SNAPSHOT.jar", "/plugins")
-                    .cmd("java", "-jar", "spigot-1.18.jar", "nogui")
-                    .build()))
-        .waitingFor(Wait.forLogMessage(".*Done.*", 1))
-        .withStartupTimeout(Duration.ofSeconds(60));
+    private GenericContainer container;
+
+    @BeforeEach
+    public void beforeEach() throws IOException {
+        final Properties properties = new Properties();
+        properties.load(getClass().getClassLoader().getResourceAsStream("project.properties"));
+        String implementationVersion = properties.getProperty("plugin.version");
+        String javaVersion = properties.getProperty("java.version");
+        String mcVersion = properties.getProperty("mc.version");
+
+        container = new GenericContainer(
+            new ImageFromDockerfile()
+                .withFileFromClasspath("staffplusplus-core-" + implementationVersion + ".jar", "/docker/staffplusplus-core-" + implementationVersion + ".jar")
+                .withFileFromClasspath("spigot-" + mcVersion + ".jar", "/docker/spigot-" + mcVersion + ".jar")
+                .withFileFromClasspath("eula.txt", "/docker/eula.txt")
+                .withFileFromClasspath("server.properties", "/docker/server.properties")
+                .withDockerfileFromBuilder(builder ->
+                    builder
+                        .from("openjdk:" + javaVersion)
+                        .copy("spigot-" + mcVersion + ".jar", ".")
+                        .copy("eula.txt", ".")
+                        .copy("server.properties", ".")
+                        .run("chmod", "777", "spigot-" + mcVersion + ".jar")
+                        .run("mkdir", "plugins")
+                        .copy("staffplusplus-core-" + implementationVersion + ".jar", "/plugins")
+                        .cmd("java", "-jar", "spigot-" + mcVersion + ".jar", "nogui")
+                        .build()))
+            .waitingFor(Wait.forLogMessage(".*Done.*", 1))
+            .withStartupTimeout(Duration.ofSeconds(60));
+        container.start();
+    }
 
     @Test
     public void run() {
