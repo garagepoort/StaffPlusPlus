5c5db4bd7561fb1de7c8ac66a8a20af51d175de7
David Maes <david.s.maes@gmail.com>
2021-12-10 23:38:34 +0100

    Add player note creation/deletion from queue

--- src/main/java/net/shortninja/staffplus/core/domain/staff/playernotes/gui/cmd/PrivateNoteCmd.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/playernotes/gui/cmd/PrivateNoteCmd.java
@@ -3,6 +3,7 @@ package net.shortninja.staffplus.core.domain.staff.playernotes.gui.cmd;
 import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocMultiProvider;
 import net.shortninja.staffplus.core.application.config.Messages;
+import net.shortninja.staffplus.core.common.Constants;
 import net.shortninja.staffplus.core.common.JavaUtils;
 import net.shortninja.staffplus.core.common.cmd.AbstractCmd;
 import net.shortninja.staffplus.core.common.cmd.Command;
@@ -12,13 +13,16 @@ import net.shortninja.staffplus.core.common.permissions.PermissionHandler;
 import net.shortninja.staffplus.core.common.utils.BukkitUtils;
 import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplus.core.domain.staff.playernotes.PlayerNoteService;
+import net.shortninja.staffplusplus.session.SppInteractor;
 import net.shortninja.staffplusplus.session.SppPlayer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static net.shortninja.staffplus.core.common.cmd.PlayerRetrievalStrategy.BOTH;
@@ -51,7 +55,11 @@ public class PrivateNoteCmd extends AbstractCmd {
 
     @Override
     protected boolean executeCmd(CommandSender sender, String alias, String[] args, SppPlayer targetPlayer, Map<String, String> optionalParameters) {
-        bukkitUtils.runTaskAsync(sender, () -> playerNoteService.createNote(sender, JavaUtils.compileWords(args, 1), targetPlayer, true));
+        UUID senderUuid = sender instanceof Player ? ((Player) sender).getUniqueId() : Constants.CONSOLE_UUID;
+        String senderName = sender instanceof Player ? sender.getName() : "Console";
+        SppInteractor sppInteractor = new SppInteractor(senderUuid, senderName, sender);
+
+        bukkitUtils.runTaskAsync(sender, () -> playerNoteService.createNote(sppInteractor, JavaUtils.compileWords(args, 1), targetPlayer, true));
         return true;
     }
 
