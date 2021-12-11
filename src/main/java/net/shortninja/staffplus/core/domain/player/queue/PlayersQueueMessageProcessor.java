70df91d280cc084984acd3b4c6082c08e40679a8
David Maes <david.s.maes@gmail.com>
2021-12-11 15:21:20 +0100

    Add update players list

--- src/main/java/net/shortninja/staffplus/core/domain/player/queue/PlayersQueueMessageProcessor.java
+++ src/main/java/net/shortninja/staffplus/core/domain/player/queue/PlayersQueueMessageProcessor.java
@@ -1,7 +1,8 @@
-package net.shortninja.staffplus.core.domain.staff.playernotes.queue;
+package net.shortninja.staffplus.core.domain.player.queue;
 
 import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocMulti;
+import net.shortninja.staffplus.core.application.config.Options;
 import net.shortninja.staffplus.core.application.queue.QueueMessageListener;
 import net.shortninja.staffplus.core.application.queue.QueueMessageProcessor;
 import net.shortninja.staffplus.core.application.queue.QueueRepository;
@@ -9,19 +10,24 @@ import net.shortninja.staffplus.core.common.bungee.GsonParser;
 import net.shortninja.staffplus.core.domain.synchronization.ServerSyncConfig;
 import net.shortninja.staffplus.core.domain.synchronization.ServerSyncConfiguration;
 
+import java.util.Collections;
 import java.util.List;
 
 // Only run when the webui is enabled.
 //  Only the web interface will communicate through the queue for now
 @IocBean(conditionalOnProperty = "webui-module.enabled=true && storage.type=mysql")
-public class PlayerNoteQueueMessageProcessor extends QueueMessageProcessor {
-    private static final String PROCESSING_GROUP = "player-note/";
+public class PlayersQueueMessageProcessor extends QueueMessageProcessor {
 
-    public PlayerNoteQueueMessageProcessor(QueueRepository queueRepository,
-                                           GsonParser gsonParser,
-                                           @IocMulti(QueueMessageListener.class) List<QueueMessageListener> listeners,
-                                           ServerSyncConfiguration serverSyncConfiguration) {
+    private static final String PROCESSING_GROUP = "players/";
+    private final ServerSyncConfig serverSyncConfig;
+
+    public PlayersQueueMessageProcessor(QueueRepository queueRepository,
+                                        GsonParser gsonParser,
+                                        @IocMulti(QueueMessageListener.class) List<QueueMessageListener> listeners,
+                                        ServerSyncConfiguration serverSyncConfiguration,
+                                        Options options) {
         super(queueRepository, gsonParser, listeners, serverSyncConfiguration);
+        serverSyncConfig = new ServerSyncConfig(Collections.singletonList(options.serverName));
     }
 
     @Override
@@ -31,7 +37,6 @@ public class PlayerNoteQueueMessageProcessor extends QueueMessageProcessor {
 
     @Override
     protected ServerSyncConfig getServerSyncConfig() {
-        return serverSyncConfiguration.notesSyncServers;
+        return serverSyncConfig;
     }
-
 }
