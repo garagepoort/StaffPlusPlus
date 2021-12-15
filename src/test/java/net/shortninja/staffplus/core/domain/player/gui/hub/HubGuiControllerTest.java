8695875d17e2bb030c0373a45078ef8d0c0485e3
David Maes <david.s.maes@gmail.com>
2021-12-15 02:00:06 +0100

    fix test

--- src/test/java/net/shortninja/staffplus/core/domain/player/gui/hub/HubGuiControllerTest.java
+++ src/test/java/net/shortninja/staffplus/core/domain/player/gui/hub/HubGuiControllerTest.java
@@ -36,32 +36,6 @@ class HubGuiControllerTest extends AbstractGuiTemplateTest {
     }
 
     private void mockConfig() {
-        when(templateConfigResolver.get("permissions:reports.manage.view")).thenReturn(VIEW_REPORTS_PERMISSION);
-        when(permissionHandler.has(player, VIEW_REPORTS_PERMISSION)).thenReturn(true);
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.name")).thenReturn("&eGUI Hub");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.reports-gui")).thenReturn(true);
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.reports-title")).thenReturn("&bUnresolved reports");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.reports-lore")).thenReturn("&7Shows all open reports.");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.my-reports-title")).thenReturn("&bReports assigned to you");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.my-reports-lore")).thenReturn("&7Shows reports currently assigned to you.");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.assigned-reports-title")).thenReturn("&bReports In Progress");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.assigned-reports-lore")).thenReturn("&7Shows reports that are currently being worked on.");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.closed-reports-title")).thenReturn("&bClosed reports");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.closed-reports-lore")).thenReturn("&7History of all closed reports.");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.miner-gui")).thenReturn(true);
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.miner-name")).thenReturn("&bMiner GUI");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.miner-lore")).thenReturn("&7Shows all players under the set Y value.");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.protected-areas-gui")).thenReturn(true);
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.protected-areas-name")).thenReturn("&bProtected Areas GUI");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.protected-areas-lore")).thenReturn("&7Shows all currently set protected areas.");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.ban-gui")).thenReturn(true);
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.ban-name")).thenReturn("&bBanned players GUI");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.ban-lore")).thenReturn("&7Shows all currently banned players.");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.mute-gui")).thenReturn(true);
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.mute-name")).thenReturn("&bMuted players GUI");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.mute-lore")).thenReturn("&7Shows all currently muted players.");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.investigation-gui")).thenReturn(true);
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.investigation-name")).thenReturn("&bInvestigations GUI");
-        when(templateConfigResolver.get("staffmode-modules:modules.gui-module.investigation-lore")).thenReturn("&7Shows all investigations.");
+        when(permissionHandler.has(player, "staff.reports.manage.view")).thenReturn(true);
     }
 }
