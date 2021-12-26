cc2e5ca5e220869dc926127b4f47532224a58d16
David Maes <david.s.maes@gmail.com>
2021-12-26 14:58:11 +0100

    Add translation for report status

--- src/main/java/net/shortninja/staffplus/core/domain/staff/reporting/gui/chat/ReportMessageUtil.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/reporting/gui/chat/ReportMessageUtil.java
@@ -1,12 +1,21 @@
 package net.shortninja.staffplus.core.domain.staff.reporting.gui.chat;
 
+import be.garagepoort.mcioc.IocBean;
+import net.shortninja.staffplus.core.application.config.Messages;
 import net.shortninja.staffplus.core.domain.staff.reporting.bungee.dto.ReportBungeeDto;
 import net.shortninja.staffplusplus.reports.IReport;
 import net.shortninja.staffplusplus.reports.ReportStatus;
 
+@IocBean
 public class ReportMessageUtil {
 
-    public static String replaceReportPlaceholders(String message, IReport report) {
+    private final Messages messages;
+
+    public ReportMessageUtil(Messages messages) {
+        this.messages = messages;
+    }
+
+    public String replaceReportPlaceholders(String message, IReport report) {
         return replaceReportPlaceholders(message,
             report.getStaffName(),
             report.getReporterName(),
@@ -16,7 +25,7 @@ public class ReportMessageUtil {
             report.getCloseReason());
     }
 
-    public static String replaceReportPlaceholders(String message, ReportBungeeDto report) {
+    public String replaceReportPlaceholders(String message, ReportBungeeDto report) {
         return replaceReportPlaceholders(message,
             report.getStaffName(),
             report.getReporterName(),
@@ -26,7 +35,7 @@ public class ReportMessageUtil {
             report.getCloseReason());
     }
 
-    public static String replaceReportPlaceholders(String message, String staff, IReport report) {
+    public String replaceReportPlaceholders(String message, String staff, IReport report) {
         return replaceReportPlaceholders(message,
             staff,
             report.getReporterName(),
@@ -36,7 +45,7 @@ public class ReportMessageUtil {
             report.getCloseReason());
     }
 
-    public static String replaceReportPlaceholders(String message, String staff, ReportBungeeDto report) {
+    public String replaceReportPlaceholders(String message, String staff, ReportBungeeDto report) {
         return replaceReportPlaceholders(message,
             staff,
             report.getReporterName(),
@@ -46,16 +55,29 @@ public class ReportMessageUtil {
             report.getCloseReason());
     }
 
-    public static String replaceReportPlaceholders(String message, String staff, String reporterName, String culpritName, String reason, ReportStatus reportStatus, String closeReason) {
+    public String replaceReportPlaceholders(String message, String staff, String reporterName, String culpritName, String reason, ReportStatus reportStatus, String closeReason) {
         String result = message;
         if (staff != null) result = result.replace("%staff%", staff);
         if (reporterName != null) result = result.replace("%reporter%", reporterName);
         if (culpritName != null) result = result.replace("%culprit%", culpritName);
         if (culpritName == null) result = result.replace("%culprit%", "Unknown");
         if (reason != null) result = result.replace("%reason%", reason);
-        if (reportStatus != null) result = result.replace("%status%", reportStatus.name());
+        if (reportStatus != null) result = result.replace("%status%", translateStatus(reportStatus));
         if (closeReason != null) result = result.replace("%close_reason%", closeReason);
         return result;
     }
 
+    public String translateStatus(ReportStatus reportStatus) {
+        switch (reportStatus) {
+            case OPEN:
+                return messages.reportOpenStatus;
+            case IN_PROGRESS:
+                return messages.reportInProgressStatus;
+            case RESOLVED:
+                return messages.reportResolvedStatus;
+            case REJECTED:
+                return messages.reportRejectedStatus;
+        }
+        return "Unknown";
+    }
 }
