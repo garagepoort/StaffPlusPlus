b7fbd2d46f33e859117204db2b3c0392b7986f9f
David Maes <david.s.maes@gmail.com>
2021-12-30 02:05:35 +0100

    Add my-mutes-cmd

---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/mute/gui/MuteNotifierListener.java
@@ -0,0 +1,59 @@
+package net.shortninja.staffplus.core.domain.staff.mute.gui;
+
+import be.garagepoort.mcioc.IocBean;
+import be.garagepoort.mcioc.IocListener;
+import be.garagepoort.mcioc.configuration.ConfigProperty;
+import me.rayzr522.jsonmessage.JSONMessage;
+import net.shortninja.staffplus.core.common.JavaUtils;
+import net.shortninja.staffplus.core.common.StaffPlusPlusJoinedEvent;
+import net.shortninja.staffplus.core.common.permissions.PermissionHandler;
+import net.shortninja.staffplus.core.common.utils.BukkitUtils;
+import net.shortninja.staffplus.core.domain.staff.mute.Mute;
+import net.shortninja.staffplus.core.domain.staff.mute.MuteService;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+import java.util.Optional;
+
+@IocBean(conditionalOnProperty = "mute-module.enabled=true")
+@IocListener
+public class MuteNotifierListener implements Listener {
+
+    @ConfigProperty("%lang%:mute-notify")
+    private String muteNotifyMessage;
+    @ConfigProperty("commands:my-mutes")
+    private String myMutesCmd;
+    @ConfigProperty("permissions:view-my-mutes")
+    private String myMutesPermission;
+
+    private final PermissionHandler permission;
+    private final BukkitUtils bukkitUtils;
+    private final MuteService muteService;
+
+    public MuteNotifierListener(PermissionHandler permission, BukkitUtils bukkitUtils, MuteService muteService) {
+        this.permission = permission;
+        this.bukkitUtils = bukkitUtils;
+        this.muteService = muteService;
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void notifyMuted(StaffPlusPlusJoinedEvent event) {
+        bukkitUtils.runTaskAsync(() -> {
+            Optional<Mute> mute = muteService.getMuteByMutedUuid(event.getPlayer().getUniqueId());
+            if (mute.isPresent() && !mute.get().isSoftMute()) {
+                sendMessage(event);
+            }
+        });
+    }
+
+    private void sendMessage(StaffPlusPlusJoinedEvent event) {
+        JSONMessage message = JavaUtils.buildClickableMessage(
+            muteNotifyMessage,
+            "View your mute!",
+            "Click to view your mute",
+            myMutesCmd,
+            permission.has(event.getPlayer(), myMutesPermission));
+        message.send(event.getPlayer());
+    }
+}
