703e979cacb167a1444d61674c0319e7ea5ad69a
David Maes <david.s.maes@gmail.com>
2022-01-10 01:34:26 +0100

    Fix migration of large data

--- src/main/java/net/shortninja/staffplus/core/application/database/migrations/common/V80_AddNamesToAppealsTableMigration.java
+++ src/main/java/net/shortninja/staffplus/core/application/database/migrations/common/V80_AddNamesToAppealsTableMigration.java
@@ -1,13 +1,12 @@
 package net.shortninja.staffplus.core.application.database.migrations.common;
 
 import be.garagepoort.mcsqlmigrations.Migration;
-import org.bukkit.Bukkit;
+import net.shortninja.staffplus.core.StaffPlus;
+import net.shortninja.staffplus.core.common.utils.DatabaseUtil;
+import net.shortninja.staffplus.core.domain.player.PlayerManager;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class V80_AddNamesToAppealsTableMigration implements Migration {
     @Override
@@ -18,11 +17,10 @@ public class V80_AddNamesToAppealsTableMigration implements Migration {
         statements.add(addAppealerName);
         statements.add(addResolverName);
 
-        List<String> updates = Arrays.stream(Bukkit.getOfflinePlayers()).flatMap(p -> Stream.of(
-            String.format("UPDATE sp_appeals set appealer_name='%s' WHERE appealer_uuid='%s';", p.getName(), p.getUniqueId()),
-            String.format("UPDATE sp_appeals set resolver_name='%s' WHERE resolver_uuid='%s';", p.getName(), p.getUniqueId())))
-            .collect(Collectors.toList());
-        statements.addAll(updates);
+
+        PlayerManager playerManager = StaffPlus.get().getIocContainer().get(PlayerManager.class);
+        statements.addAll(DatabaseUtil.createMigrateNameStatements(playerManager, "sp_appeals", "appealer_name","appealer_uuid"));
+        statements.addAll(DatabaseUtil.createMigrateNameStatements(playerManager, "sp_appeals", "resolver_name","resolver_uuid"));
         return statements;
     }
 
