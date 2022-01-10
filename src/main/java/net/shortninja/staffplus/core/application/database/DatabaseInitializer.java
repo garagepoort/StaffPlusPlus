1d752896b46c60b234d9e5b98bc785cf9255e14f
David Maes <david.s.maes@gmail.com>
2022-01-10 01:34:26 +0100

    Fix migration of large data

--- src/main/java/net/shortninja/staffplus/core/application/database/DatabaseInitializer.java
+++ src/main/java/net/shortninja/staffplus/core/application/database/DatabaseInitializer.java
@@ -5,13 +5,18 @@ import be.garagepoort.mcsqlmigrations.Migrations;
 import be.garagepoort.mcsqlmigrations.SqlConnectionProvider;
 import net.shortninja.staffplus.core.application.config.Options;
 import net.shortninja.staffplus.core.domain.actions.database.StoredCommandRepository;
+import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplus.core.domain.player.ip.database.PlayerIpRepository;
 
 @IocBean(priority = true)
 public class DatabaseInitializer {
 
     // The parameters injected here are to ensure the migrations can use them, do not remove them
-    public DatabaseInitializer(SqlConnectionProvider sqlConnectionProvider, PlayerIpRepository playerIpRepository, Options options, StoredCommandRepository storedCommandRepository) {
+    public DatabaseInitializer(SqlConnectionProvider sqlConnectionProvider,
+                               PlayerIpRepository playerIpRepository,
+                               Options options,
+                               StoredCommandRepository storedCommandRepository,
+                               PlayerManager playerManager) {
         new Migrations(sqlConnectionProvider).run("migrations");
     }
 
