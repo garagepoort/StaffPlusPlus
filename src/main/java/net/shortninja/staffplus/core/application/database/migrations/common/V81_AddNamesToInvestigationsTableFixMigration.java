9276e898896e2a588ad82f94f8ca9b789d57f853
David Maes <david.s.maes@gmail.com>
2022-01-10 02:09:34 +0100

    Fix migration

---
+++ src/main/java/net/shortninja/staffplus/core/application/database/migrations/common/V81_AddNamesToInvestigationsTableFixMigration.java
@@ -0,0 +1,35 @@
+package net.shortninja.staffplus.core.application.database.migrations.common;
+
+import be.garagepoort.mcsqlmigrations.Migration;
+import net.shortninja.staffplus.core.StaffPlus;
+import net.shortninja.staffplus.core.common.utils.DatabaseUtil;
+import net.shortninja.staffplus.core.domain.player.PlayerManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class V81_AddNamesToInvestigationsTableFixMigration implements Migration {
+    @Override
+    public List<String> getStatements() {
+        String dropInvestigatorName = "ALTER TABLE sp_investigations DROP COLUMN investigator_name;";
+        String dropInvestigatedName = "ALTER TABLE sp_investigations DROP COLUMN investigated_name;";
+        String addInvestigatorName = "ALTER TABLE sp_investigations ADD COLUMN investigator_name VARCHAR(32) NOT NULL DEFAULT 'Unknown';";
+        String addInvestigatedName = "ALTER TABLE sp_investigations ADD COLUMN investigated_name VARCHAR(32) NULL;";
+        List<String> statements = new ArrayList<>();
+        statements.add(dropInvestigatedName);
+        statements.add(dropInvestigatorName);
+
+        statements.add(addInvestigatedName);
+        statements.add(addInvestigatorName);
+
+        PlayerManager playerManager = StaffPlus.get().getIocContainer().get(PlayerManager.class);
+        statements.addAll(DatabaseUtil.createMigrateNameStatements(playerManager, "sp_investigations", "investigator_name","investigator_uuid"));
+        statements.addAll(DatabaseUtil.createMigrateNameStatements(playerManager, "sp_investigations", "investigated_name","investigated_uuid"));
+        return statements;
+    }
+
+    @Override
+    public int getVersion() {
+        return 81;
+    }
+}
