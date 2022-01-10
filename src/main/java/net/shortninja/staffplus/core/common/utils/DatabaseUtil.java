dfdb41aac433a8418f8046a1205605ba990e1687
David Maes <david.s.maes@gmail.com>
2022-01-10 01:34:26 +0100

    Fix migration of large data

--- src/main/java/net/shortninja/staffplus/core/common/utils/DatabaseUtil.java
+++ src/main/java/net/shortninja/staffplus/core/common/utils/DatabaseUtil.java
@@ -1,10 +1,17 @@
 package net.shortninja.staffplus.core.common.utils;
 
+import be.garagepoort.mcsqlmigrations.SqlConnectionProvider;
+import net.shortninja.staffplus.core.StaffPlus;
+import net.shortninja.staffplus.core.common.exceptions.DatabaseException;
+import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplusplus.common.SqlFilter;
 import net.shortninja.staffplusplus.common.SqlFilters;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,12 +29,11 @@ public class DatabaseUtil {
             })
             .collect(Collectors.joining());
 
-        if(prefixAnd) {
+        if (prefixAnd) {
             return collect;
         }
         return collect.replaceFirst(" AND", "");
     }
-
 
     public static int insertFilterValues(SqlFilters sqlFilters, PreparedStatement ps, int index) throws SQLException {
         for (SqlFilter reportFilter : sqlFilters.getSqlFilters()) {
@@ -43,5 +49,32 @@ public class DatabaseUtil {
             }
         }
         return index;
+    }
+
+    public static List<String> createMigrateNameStatements(PlayerManager playerManager, String tableName, String nameColumn, String uuid_column) {
+        List<String> statements = new ArrayList<>();
+        List<String> playerUuids = DatabaseUtil.getUuids(tableName, uuid_column);
+        playerUuids
+            .forEach(playerUuid -> playerManager.getOnOrOfflinePlayer(playerUuid)
+            .ifPresent(p -> statements.add(String.format("UPDATE " + tableName + " set " + nameColumn + "='%s' WHERE " + uuid_column + "='%s';", p.getUsername(), p.getId()))));
+        return statements;
+    }
+
+    public static List<String> getUuids(String table, String column) {
+
+        SqlConnectionProvider sqlConnectionProvider = StaffPlus.get().getIocContainer().get(SqlConnectionProvider.class);
+
+        List<String> uuids = new ArrayList<>();
+        try (Connection sql = sqlConnectionProvider.getConnection();
+             PreparedStatement ps = sql.prepareStatement("SELECT distinct " + column + " FROM " + table)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    uuids.add(rs.getString(1));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
+        return uuids;
     }
 }
