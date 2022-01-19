41042c746e2175bafa6396b273ce077749dcf672
David Maes <david.s.maes@gmail.com>
2022-01-20 00:10:51 +0100

    Merge pull request #194 from garagepoort/feature/chat_channels
    Feature/chat channels

---
+++ src/main/java/net/shortninja/staffplus/core/application/database/migrations/mysql/V82_CreateChatChannelsTableMigration.java
@@ -0,0 +1,32 @@
+package net.shortninja.staffplus.core.application.database.migrations.mysql;
+
+import be.garagepoort.mcsqlmigrations.Migration;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class V82_CreateChatChannelsTableMigration implements Migration {
+    @Override
+    public List<String> getStatements() {
+        String chatChannels = "CREATE TABLE IF NOT EXISTS sp_chat_channels (  " +
+            "ID INT NOT NULL AUTO_INCREMENT,  " +
+            "chat_prefix VARCHAR(255) NOT NULL, " +
+            "chat_line VARCHAR(255) NOT NULL, " +
+            "channel_name VARCHAR(255) NOT NULL, " +
+            "channel_id VARCHAR(36) NOT NULL, " +
+            "type VARCHAR(36) NOT NULL, " +
+            "server_name VARCHAR(255) NOT NULL, " +
+            "PRIMARY KEY (ID)) ENGINE = InnoDB;";
+        String members = "CREATE TABLE IF NOT EXISTS sp_chat_channel_members (  " +
+            "ID INT NOT NULL AUTO_INCREMENT,  " +
+            "channel_id INT NOT NULL, " +
+            "player_uuid VARCHAR(36) NOT NULL, " +
+            "PRIMARY KEY (ID)) ENGINE = InnoDB;";
+        return Arrays.asList(chatChannels, members);
+    }
+
+    @Override
+    public int getVersion() {
+        return 82;
+    }
+}
