4994229fa01ee0778e45404ac79196c7df03f41b
David Maes <david.s.maes@gmail.com>
2022-02-10 14:03:43 +0100

    Fix list vanish

--- src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/ListVanishStrategy.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/ListVanishStrategy.java
@@ -4,9 +4,15 @@ import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocMultiProvider;
 import net.shortninja.staffplus.core.application.config.Messages;
 import net.shortninja.staffplus.core.common.IProtocolService;
+import net.shortninja.staffplus.core.common.permissions.PermissionHandler;
+import net.shortninja.staffplus.core.domain.player.PlayerManager;
+import net.shortninja.staffplusplus.session.SessionManager;
+import net.shortninja.staffplusplus.session.SppPlayer;
 import net.shortninja.staffplusplus.vanish.VanishType;
 import org.apache.commons.lang.StringUtils;
 import org.bukkit.entity.Player;
+
+import java.util.stream.Stream;
 
 @IocBean
 @IocMultiProvider(VanishStrategy.class)
@@ -15,11 +21,17 @@ public class ListVanishStrategy implements VanishStrategy {
     private final VanishConfiguration vanishConfiguration;
     private final Messages messages;
     private final IProtocolService protocolService;
+    private final PermissionHandler permission;
+    private final SessionManager sessionManager;
+    private final PlayerManager playerManager;
 
-    public ListVanishStrategy(VanishConfiguration vanishConfiguration, Messages messages, IProtocolService protocolService) {
+    public ListVanishStrategy(VanishConfiguration vanishConfiguration, Messages messages, IProtocolService protocolService, PermissionHandler permission, SessionManager sessionManager, PlayerManager playerManager) {
         this.vanishConfiguration = vanishConfiguration;
         this.messages = messages;
         this.protocolService = protocolService;
+        this.permission = permission;
+        this.sessionManager = sessionManager;
+        this.playerManager = playerManager;
     }
 
     @Override
@@ -45,7 +57,14 @@ public class ListVanishStrategy implements VanishStrategy {
 
     @Override
     public void updateVanish(Player player) {
-        // no implementation for list vanish
+        if (!permission.has(player, vanishConfiguration.permissionSeeVanished)) {
+            sessionManager.getAll().stream()
+                .filter(s -> s.getVanishType() == VanishType.LIST)
+                .map(s -> playerManager.getOnlinePlayer(s.getUuid()))
+                .flatMap(optional -> optional.map(Stream::of).orElseGet(Stream::empty))
+                .map(SppPlayer::getPlayer)
+                .forEach(p -> protocolService.getVersionProtocol().listVanish(p.getPlayer(), true));
+        }
     }
 
     @Override
