a5a4406f3f13b6687873d73ccc99d81048fd5275
David Maes <david.s.maes@gmail.com>
2022-02-17 23:07:22 +0100

    Add bungee for mention and namechanged alerts

---
+++ src/main/java/net/shortninja/staffplus/core/domain/player/namechanged/bungee/NameChangedBungeeReceiver.java
@@ -0,0 +1,41 @@
+package net.shortninja.staffplus.core.domain.player.namechanged.bungee;
+
+import be.garagepoort.mcioc.IocBean;
+import be.garagepoort.mcioc.IocMessageListener;
+import be.garagepoort.mcioc.configuration.ConfigProperty;
+import be.garagepoort.mcioc.configuration.ConfigTransformer;
+import net.shortninja.staffplus.core.common.Constants;
+import net.shortninja.staffplus.core.common.bungee.BungeeClient;
+import net.shortninja.staffplus.core.domain.synchronization.ServerSyncConfig;
+import net.shortninja.staffplus.core.domain.synchronization.ServerSyncConfigTransformer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+import java.util.Optional;
+
+import static net.shortninja.staffplus.core.common.Constants.BUNGEE_CORD_CHANNEL;
+
+@IocBean(conditionalOnProperty = "isNotEmpty(alerts-module.name-notify-bungee)")
+@IocMessageListener(channel = BUNGEE_CORD_CHANNEL)
+public class NameChangedBungeeReceiver implements PluginMessageListener {
+
+    @ConfigProperty("alerts-module.name-notify-bungee")
+    @ConfigTransformer(ServerSyncConfigTransformer.class)
+    private ServerSyncConfig syncServers;
+
+    private final BungeeClient bungeeClient;
+
+    public NameChangedBungeeReceiver(BungeeClient bungeeClient) {
+        this.bungeeClient = bungeeClient;
+    }
+
+    @Override
+    public void onPluginMessageReceived(String channel, Player player, byte[] message) {
+        Optional<NameChangeBungeeDto> nameChangeBungeeDto = bungeeClient.handleReceived(channel, Constants.BUNGEE_NAME_CHANGED_ALERT_CHANNEL, message, NameChangeBungeeDto.class);
+
+        if (nameChangeBungeeDto.isPresent() && syncServers.matchesServer(nameChangeBungeeDto.get().getServerName())) {
+            Bukkit.getPluginManager().callEvent(new NameChangedBungeeEvent(nameChangeBungeeDto.get()));
+        }
+    }
+}
--- src/main/java/net/shortninja/staffplus/core/domain/staff/alerts/handlers/NameChangeAlertConsoleHandler.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/alerts/handlers/NameChangeAlertConsoleHandler.java
@@ -5,10 +5,16 @@ import be.garagepoort.mcioc.IocListener;
 import net.shortninja.staffplus.core.StaffPlus;
 import net.shortninja.staffplus.core.application.config.Messages;
 import net.shortninja.staffplus.core.common.permissions.PermissionHandler;
+import net.shortninja.staffplus.core.domain.player.PlayerManager;
+import net.shortninja.staffplus.core.domain.player.namechanged.bungee.NameChangeBungeeDto;
+import net.shortninja.staffplus.core.domain.player.namechanged.bungee.NameChangedBungeeEvent;
 import net.shortninja.staffplus.core.domain.staff.alerts.config.AlertsConfiguration;
 import net.shortninja.staffplusplus.chat.NameChangeEvent;
+import net.shortninja.staffplusplus.session.SppPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.Optional;
 
 @IocBean(conditionalOnProperty = "alerts-module.name-notify-console=true")
 @IocListener
@@ -17,11 +23,13 @@ public class NameChangeAlertConsoleHandler implements Listener {
     private final Messages messages;
     private final AlertsConfiguration alertsConfiguration;
     private final PermissionHandler permission;
+    private final PlayerManager playerManager;
 
-    public NameChangeAlertConsoleHandler(Messages messages, AlertsConfiguration alertsConfiguration, PermissionHandler permission) {
+    public NameChangeAlertConsoleHandler(Messages messages, AlertsConfiguration alertsConfiguration, PermissionHandler permission, PlayerManager playerManager) {
         this.messages = messages;
         this.alertsConfiguration = alertsConfiguration;
         this.permission = permission;
+        this.playerManager = playerManager;
     }
 
     @EventHandler
@@ -30,8 +38,26 @@ public class NameChangeAlertConsoleHandler implements Listener {
             return;
         }
 
-        String message = messages.alertsName.replace("%old%", nameChangeEvent.getOldName()).replace("%new%", nameChangeEvent.getNewName());
-        StaffPlus.get().getLogger().info(message);
+        log(nameChangeEvent.getOldName(), nameChangeEvent.getServerName(), nameChangeEvent.getNewName());
     }
 
+    @EventHandler
+    public void handle(NameChangedBungeeEvent nameChangeEvent) {
+        NameChangeBungeeDto nameChangeBungeeDto = nameChangeEvent.getNameChangeBungeeDto();
+        Optional<SppPlayer> sppPlayer = playerManager.getOnOrOfflinePlayer(nameChangeBungeeDto.getPlayerUuid());
+        if (sppPlayer.isPresent()) {
+            if (permission.has(sppPlayer.get().getOfflinePlayer(), alertsConfiguration.permissionNameChangeBypass)) {
+                return;
+            }
+            log(nameChangeBungeeDto.getOldName(), nameChangeBungeeDto.getServerName(), nameChangeBungeeDto.getNewName());
+        }
+    }
+
+    private void log(String oldName, String serverName, String newName) {
+        String message = messages.alertsName
+            .replace("%old%", oldName)
+            .replace("%server%", serverName)
+            .replace("%new%", newName);
+        StaffPlus.get().getLogger().info(message);
+    }
 }
