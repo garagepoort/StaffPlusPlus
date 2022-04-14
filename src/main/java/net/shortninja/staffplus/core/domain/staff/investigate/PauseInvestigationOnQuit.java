9daccc46e668d762a987e9cd4324f798bc8cfaf8
David Maes <david.s.maes@gmail.com>
2022-04-14 23:45:13 +0200

    Add staffhub command

--- src/main/java/net/shortninja/staffplus/core/domain/staff/investigate/PauseInvestigationOnQuit.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/investigate/PauseInvestigationOnQuit.java
@@ -1,8 +1,9 @@
 package net.shortninja.staffplus.core.domain.staff.investigate;
 
-import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocListener;
 import be.garagepoort.mcioc.IocMultiProvider;
+import be.garagepoort.mcioc.TubingPlugin;
+import be.garagepoort.mcioc.load.BeforeTubingReload;
 import net.shortninja.staffplus.core.StaffPlus;
 import net.shortninja.staffplus.core.application.bootstrap.PluginDisable;
 import net.shortninja.staffplus.core.common.utils.BukkitUtils;
@@ -13,8 +14,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 @IocListener
-@IocMultiProvider(PluginDisable.class)
-public class PauseInvestigationOnQuit implements Listener, PluginDisable {
+@IocMultiProvider({PluginDisable.class, BeforeTubingReload.class})
+public class PauseInvestigationOnQuit implements Listener, PluginDisable, BeforeTubingReload {
 
     private final InvestigationService investigationService;
     private final InvestigationsRepository investigationsRepository;
@@ -35,6 +36,12 @@ public class PauseInvestigationOnQuit implements Listener, PluginDisable {
     @Override
     public void disable(StaffPlus staffPlus) {
         //disable all when plugin disabled. To ensure pausing all investigations when the server restarts.
+        investigationsRepository.pauseAllInvestigations();
+    }
+
+    @Override
+    public void execute(TubingPlugin tubingPlugin) {
+        //disable all when plugin reloads. To ensure pausing all investigations when the server restarts.
         investigationsRepository.pauseAllInvestigations();
     }
 }
