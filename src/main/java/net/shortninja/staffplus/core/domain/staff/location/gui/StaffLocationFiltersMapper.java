6a8d4c4e5adb7fdbab28980c3e592d02d8a624ea
DavidMaes <david.s.maes@gmail.com>
2022-05-12 22:11:54 +0200

    Add fix staff locations

---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/location/gui/StaffLocationFiltersMapper.java
@@ -0,0 +1,31 @@
+package net.shortninja.staffplus.core.domain.staff.location.gui;
+
+import be.garagepoort.mcioc.IocBean;
+import net.shortninja.staffplusplus.stafflocations.StaffLocationFilters.StaffLocationFiltersBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+
+@IocBean
+public class StaffLocationFiltersMapper {
+
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String CREATOR = "creator";
+
+    public List<String> getFilterKeys() {
+        return Arrays.asList(ID, NAME, CREATOR);
+    }
+
+    public void map(String key, String value, StaffLocationFiltersBuilder filtersBuilder) {
+        if (key.equalsIgnoreCase(ID)) {
+            filtersBuilder.id(Integer.parseInt(value));
+        }
+        if (key.equalsIgnoreCase(NAME)) {
+            filtersBuilder.name(value);
+        }
+        if (key.equalsIgnoreCase(CREATOR)) {
+            filtersBuilder.creator(value);
+        }
+    }
+}
