78ab7df04f1bdf4e437d5e497d5d09827e662a62
DavidMaes <david.s.maes@gmail.com>
2022-05-17 01:28:34 +0200

    Add staff location icons

--- src/main/java/net/shortninja/staffplus/core/domain/staff/location/gui/cmd/CreateStaffLocationCmd.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/location/gui/cmd/CreateStaffLocationCmd.java
@@ -2,6 +2,8 @@ package net.shortninja.staffplus.core.domain.staff.location.gui.cmd;
 
 import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocMultiProvider;
+import be.garagepoort.mcioc.tubinggui.GuiActionBuilder;
+import be.garagepoort.mcioc.tubinggui.GuiActionService;
 import net.shortninja.staffplus.core.application.config.Messages;
 import net.shortninja.staffplus.core.common.JavaUtils;
 import net.shortninja.staffplus.core.common.cmd.AbstractCmd;
@@ -9,7 +11,6 @@ import net.shortninja.staffplus.core.common.cmd.Command;
 import net.shortninja.staffplus.core.common.cmd.CommandService;
 import net.shortninja.staffplus.core.common.cmd.SppCommand;
 import net.shortninja.staffplus.core.common.permissions.PermissionHandler;
-import net.shortninja.staffplus.core.domain.staff.location.StaffLocationService;
 import net.shortninja.staffplusplus.session.SppPlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -27,17 +28,20 @@ import java.util.Optional;
 @IocMultiProvider(SppCommand.class)
 public class CreateStaffLocationCmd extends AbstractCmd {
 
-    private final StaffLocationService staffLocationService;
+    private final GuiActionService guiActionService;
 
-    public CreateStaffLocationCmd(Messages messages, PermissionHandler permissionHandler, CommandService commandService, StaffLocationService staffLocationService) {
+    public CreateStaffLocationCmd(Messages messages, PermissionHandler permissionHandler, CommandService commandService, GuiActionService guiActionService) {
         super(messages, permissionHandler, commandService);
-        this.staffLocationService = staffLocationService;
+        this.guiActionService = guiActionService;
     }
 
     @Override
     protected boolean executeCmd(CommandSender sender, String alias, String[] args, SppPlayer player, Map<String, String> optionalParameters) {
         Player initiator = validateIsPlayer(sender);
-        staffLocationService.saveLocation(initiator, JavaUtils.compileWords(args, 0));
+        String locationName = JavaUtils.compileWords(args, 0);
+        guiActionService.executeAction(initiator, GuiActionBuilder.fromAction("staff-locations/create-flow/select-icon")
+            .param("locationName", locationName)
+            .build());
         return true;
     }
 
