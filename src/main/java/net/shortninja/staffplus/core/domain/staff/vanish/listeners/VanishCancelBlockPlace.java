187e0e7ef4d372479e75c43268622fda26246af6
DavidMaes <david.s.maes@gmail.com>
2022-05-31 00:57:48 +0200

    Add vanish options

---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/listeners/VanishCancelBlockPlace.java
@@ -0,0 +1,27 @@
+package net.shortninja.staffplus.core.domain.staff.vanish.listeners;
+
+import be.garagepoort.mcioc.IocListener;
+import net.shortninja.staffplus.core.application.session.OnlinePlayerSession;
+import net.shortninja.staffplus.core.application.session.OnlineSessionsManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+@IocListener(conditionalOnProperty = "vanish-module.block-place=false")
+public class VanishCancelBlockPlace implements Listener {
+    private final OnlineSessionsManager sessionManager;
+
+    public VanishCancelBlockPlace(OnlineSessionsManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onPlace(BlockPlaceEvent event) {
+        OnlinePlayerSession session = sessionManager.get(event.getPlayer());
+
+        if (session.isVanished()) {
+            event.setCancelled(true);
+        }
+    }
+}
