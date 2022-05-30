dceb4f8b0cb1516283e0aca010adf3ac23dcb642
DavidMaes <david.s.maes@gmail.com>
2022-05-31 00:57:48 +0200

    Add vanish options

---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/listeners/VanishCancelDropItem.java
@@ -0,0 +1,26 @@
+package net.shortninja.staffplus.core.domain.staff.vanish.listeners;
+
+import be.garagepoort.mcioc.IocListener;
+import net.shortninja.staffplus.core.application.session.OnlinePlayerSession;
+import net.shortninja.staffplus.core.application.session.OnlineSessionsManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+@IocListener(conditionalOnProperty = "vanish-module.item-drop=false")
+public class VanishCancelDropItem implements Listener {
+    private final OnlineSessionsManager sessionManager;
+
+    public VanishCancelDropItem(OnlineSessionsManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onDrop(PlayerDropItemEvent event) {
+        OnlinePlayerSession session = sessionManager.get(event.getPlayer());
+        if (session.isVanished()) {
+            event.setCancelled(true);
+        }
+    }
+}
