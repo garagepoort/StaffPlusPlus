7529a21f47cfa55624255d0e207fe3211ddf8033
DavidMaes <david.s.maes@gmail.com>
2022-05-31 16:52:20 +0200

    Add chest interaction for vanished players

--- src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/listeners/VanishNormalChestOpening.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/listeners/VanishNormalChestOpening.java
@@ -4,48 +4,48 @@ import be.garagepoort.mcioc.IocListener;
 import be.garagepoort.mcioc.configuration.ConfigProperty;
 import net.shortninja.staffplus.core.application.session.OnlinePlayerSession;
 import net.shortninja.staffplus.core.application.session.OnlineSessionsManager;
-import net.shortninja.staffplus.core.domain.staff.chests.ChestGuiBuilder;
 import org.bukkit.block.Container;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-@IocListener(conditionalOnProperty = "vanish-module.silent-chest-opening=true")
-public class VanishSilentChestOpening implements Listener {
+@IocListener
+public class VanishNormalChestOpening implements Listener {
 
-    @ConfigProperty("vanish-module.silent-chest-interaction")
-    private boolean silentChestInteraction;
     @ConfigProperty("vanish-module.normal-chest-opening")
     private boolean normalChestOpening;
+    @ConfigProperty("vanish-module.normal-chest-interaction")
+    private boolean normalChestInteraction;
 
     private final OnlineSessionsManager sessionManager;
-    private final ChestGuiBuilder chestGuiBuilder;
 
-    public VanishSilentChestOpening(OnlineSessionsManager sessionManager, ChestGuiBuilder chestGuiBuilder) {
+    public VanishNormalChestOpening(OnlineSessionsManager sessionManager) {
         this.sessionManager = sessionManager;
-        this.chestGuiBuilder = chestGuiBuilder;
     }
 
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
 
-        if(allowWhileSneaking(player)) {
-            return;
-        }
-
         OnlinePlayerSession playerSession = sessionManager.get(player);
-        if (playerSession.isVanished() && checkingChest(event)) {
-            Container container = (Container) event.getClickedBlock().getState();
-            chestGuiBuilder.build(container, silentChestInteraction).show(player);
+
+        if (playerSession.isVanished() && checkingChest(event) && !normalChestOpening) {
             event.setCancelled(true);
         }
     }
 
-    private boolean allowWhileSneaking(Player player) {
-        return player.isSneaking() && normalChestOpening;
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onClick(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+
+        OnlinePlayerSession playerSession = sessionManager.get(player);
+
+        if (playerSession.isVanished() && !normalChestInteraction) {
+            event.setCancelled(true);
+        }
     }
 
     private boolean checkingChest(PlayerInteractEvent event) {
