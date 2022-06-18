418e40de97e64ea133999282378041dcf5a992f3
DavidMaes <david.s.maes@gmail.com>
2022-06-19 01:32:09 +0200

    Rework alerts permission

--- src/main/java/net/shortninja/staffplus/core/domain/staff/alerts/handlers/NameChangeAlertConsoleHandler.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/alerts/handlers/NameChangeAlertConsoleHandler.java
@@ -3,11 +3,9 @@ package net.shortninja.staffplus.core.domain.staff.alerts.handlers;
 import be.garagepoort.mcioc.IocListener;
 import net.shortninja.staffplus.core.StaffPlus;
 import net.shortninja.staffplus.core.application.config.messages.Messages;
-import net.shortninja.staffplus.core.common.permissions.PermissionHandler;
 import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplus.core.domain.player.namechanged.bungee.NameChangeBungeeDto;
 import net.shortninja.staffplus.core.domain.player.namechanged.bungee.NameChangedBungeeEvent;
-import net.shortninja.staffplus.core.domain.staff.alerts.config.AlertsConfiguration;
 import net.shortninja.staffplusplus.chat.NameChangeEvent;
 import net.shortninja.staffplusplus.session.SppPlayer;
 import org.bukkit.event.EventHandler;
@@ -19,23 +17,15 @@ import java.util.Optional;
 public class NameChangeAlertConsoleHandler implements Listener {
 
     private final Messages messages;
-    private final AlertsConfiguration alertsConfiguration;
-    private final PermissionHandler permission;
     private final PlayerManager playerManager;
 
-    public NameChangeAlertConsoleHandler(Messages messages, AlertsConfiguration alertsConfiguration, PermissionHandler permission, PlayerManager playerManager) {
+    public NameChangeAlertConsoleHandler(Messages messages, PlayerManager playerManager) {
         this.messages = messages;
-        this.alertsConfiguration = alertsConfiguration;
-        this.permission = permission;
         this.playerManager = playerManager;
     }
 
     @EventHandler
     public void handle(NameChangeEvent nameChangeEvent) {
-        if (permission.has(nameChangeEvent.getPlayer(), alertsConfiguration.permissionNameChangeBypass)) {
-            return;
-        }
-
         log(nameChangeEvent.getOldName(), nameChangeEvent.getServerName(), nameChangeEvent.getNewName());
     }
 
@@ -44,9 +34,6 @@ public class NameChangeAlertConsoleHandler implements Listener {
         NameChangeBungeeDto nameChangeBungeeDto = nameChangeEvent.getNameChangeBungeeDto();
         Optional<SppPlayer> sppPlayer = playerManager.getOnOrOfflinePlayer(nameChangeBungeeDto.getPlayerUuid());
         if (sppPlayer.isPresent()) {
-            if (permission.has(sppPlayer.get().getOfflinePlayer(), alertsConfiguration.permissionNameChangeBypass)) {
-                return;
-            }
             log(nameChangeBungeeDto.getOldName(), nameChangeBungeeDto.getServerName(), nameChangeBungeeDto.getNewName());
         }
     }
