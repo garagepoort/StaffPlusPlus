17851daaeeea2120d705f00a81f6e6bcc6442186
DavidMaes <david.s.maes@gmail.com>
2022-06-19 01:32:09 +0200

    Rework alerts permission

--- src/main/java/net/shortninja/staffplus/core/domain/staff/alerts/handlers/CommandDetectedAlertConsoleHandler.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/alerts/handlers/CommandDetectedAlertConsoleHandler.java
@@ -3,8 +3,6 @@ package net.shortninja.staffplus.core.domain.staff.alerts.handlers;
 import be.garagepoort.mcioc.IocListener;
 import net.shortninja.staffplus.core.StaffPlus;
 import net.shortninja.staffplus.core.application.config.messages.Messages;
-import net.shortninja.staffplus.core.common.permissions.PermissionHandler;
-import net.shortninja.staffplus.core.domain.staff.alerts.config.AlertsConfiguration;
 import net.shortninja.staffplusplus.commanddetection.CommandDetectedEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,20 +11,13 @@ import org.bukkit.event.Listener;
 public class CommandDetectedAlertConsoleHandler implements Listener {
 
     private final Messages messages;
-    private final PermissionHandler permissionHandler;
-    private final AlertsConfiguration alertsConfiguration;
 
-    public CommandDetectedAlertConsoleHandler(Messages messages, PermissionHandler permissionHandler, AlertsConfiguration alertsConfiguration) {
+    public CommandDetectedAlertConsoleHandler(Messages messages) {
         this.messages = messages;
-        this.permissionHandler = permissionHandler;
-        this.alertsConfiguration = alertsConfiguration;
     }
 
     @EventHandler
     public void handle(CommandDetectedEvent commandDetectedEvent) {
-        if (permissionHandler.has(commandDetectedEvent.getPlayer(), alertsConfiguration.permissionCommandDetectionBypass)) {
-            return;
-        }
         String message = messages.alertsCommandDetected
             .replace("%target%", commandDetectedEvent.getPlayer().getName())
             .replace("%command%", commandDetectedEvent.getCommand());
