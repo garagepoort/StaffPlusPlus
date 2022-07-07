1aa636bf50bb1d16fc4084766169dfd8332b072d
DavidMaes <david.s.maes@gmail.com>
2022-07-07 21:47:30 +0200

    Vanish cancel interaction

---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/listeners/VanishCancelInteraction.java
@@ -0,0 +1,42 @@
+package net.shortninja.staffplus.core.domain.staff.vanish.listeners;
+
+import be.garagepoort.mcioc.configuration.ConfigProperty;
+import be.garagepoort.mcioc.configuration.ConfigTransformer;
+import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitListener;
+import be.garagepoort.mcioc.tubingbukkit.config.transformers.ToMaterials;
+import net.shortninja.staffplus.core.application.session.OnlineSessionsManager;
+import net.shortninja.staffplus.core.application.session.PlayerSession;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.Set;
+
+@IocBukkitListener(conditionalOnProperty = "vanish-module.enabled=true")
+public class VanishCancelInteraction implements Listener {
+    private final OnlineSessionsManager sessionManager;
+
+    @ConfigProperty("vanish-module.cancelled-interaction-types")
+    @ConfigTransformer(ToMaterials.class)
+    private Set<Material> cancelledInteractions;
+
+    public VanishCancelInteraction(OnlineSessionsManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onInteract(PlayerInteractEvent event) {
+        if (event.getClickedBlock() != null) {
+            Player player = event.getPlayer();
+            PlayerSession playerSession = sessionManager.get(player);
+            if (playerSession.isVanished()) {
+                if (cancelledInteractions.contains(event.getClickedBlock().getBlockData().getMaterial())) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+}
