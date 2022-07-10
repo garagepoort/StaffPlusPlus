c1517806ab44628f1615a7f091e55d0c68d5a3c8
DavidMaes <david.s.maes@gmail.com>
2022-07-11 00:04:42 +0200

    Fix staff exit mode

--- src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/gui/VanishPlayersBukkitService.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/gui/VanishPlayersBukkitService.java
@@ -1,13 +1,11 @@
 package net.shortninja.staffplus.core.domain.staff.vanish.gui;
 
-import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitListener;
 import be.garagepoort.mcioc.IocMulti;
+import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitListener;
 import net.shortninja.staffplus.core.common.exceptions.BusinessException;
-import net.shortninja.staffplus.core.common.exceptions.PlayerNotFoundException;
 import net.shortninja.staffplus.core.domain.player.PlayerManager;
 import net.shortninja.staffplus.core.domain.staff.vanish.VanishConfiguration;
 import net.shortninja.staffplus.core.domain.staff.vanish.VanishStrategy;
-import net.shortninja.staffplusplus.session.SppPlayer;
 import net.shortninja.staffplusplus.vanish.VanishOffEvent;
 import net.shortninja.staffplusplus.vanish.VanishOnEvent;
 import net.shortninja.staffplusplus.vanish.VanishType;
@@ -38,30 +36,32 @@ public class VanishPlayersBukkitService implements Listener {
 
     @EventHandler
     public void onUnvanish(VanishOffEvent event) {
-        SppPlayer sppPlayer = playerManager.getOnlinePlayer(event.getPlayer().getUniqueId())
-            .orElseThrow(() -> new PlayerNotFoundException(event.getPlayer().getName()));
-        vanishStrategies.stream()
-            .filter(s -> s.getVanishType() == event.getType())
-            .forEach(v -> v.unvanish(sppPlayer));
+        playerManager.getOnlinePlayer(event.getPlayer().getUniqueId())
+            .ifPresent(sppPlayer -> {
+                vanishStrategies.stream()
+                    .filter(s -> s.getVanishType() == event.getType())
+                    .forEach(v -> v.unvanish(sppPlayer));
+            });
     }
 
     private void vanishPlayers(Player player, VanishType vanishType) {
-        SppPlayer sppPlayer = playerManager.getOnlinePlayer(player.getUniqueId())
-            .orElseThrow(() -> new PlayerNotFoundException(player.getName()));
-        vanishStrategies.stream()
-            .filter(v -> v.getVanishType() == vanishType)
-            .findFirst()
-            .orElseThrow(() -> new BusinessException("&CNo Suitable vanish strategy found for type [" + vanishType + "]"))
-            .vanish(sppPlayer);
+        playerManager.getOnlinePlayer(player.getUniqueId())
+            .ifPresent(sppPlayer -> {
+                vanishStrategies.stream()
+                    .filter(v -> v.getVanishType() == vanishType)
+                    .findFirst()
+                    .orElseThrow(() -> new BusinessException("&CNo Suitable vanish strategy found for type [" + vanishType + "]"))
+                    .vanish(sppPlayer);
+            });
     }
 
     public void updateVanish(Player player) {
-        SppPlayer sppPlayer = playerManager.getOnlinePlayer(player.getUniqueId())
-            .orElseThrow(() -> new PlayerNotFoundException(player.getName()));
-        if (!vanishConfiguration.vanishEnabled) {
-            return;
-        }
-        vanishStrategies.forEach(v -> v.updateVanish(sppPlayer));
+        playerManager.getOnlinePlayer(player.getUniqueId())
+            .ifPresent(sppPlayer -> {
+                if (!vanishConfiguration.vanishEnabled) {
+                    return;
+                }
+                vanishStrategies.forEach(v -> v.updateVanish(sppPlayer));
+            });
     }
-
 }
