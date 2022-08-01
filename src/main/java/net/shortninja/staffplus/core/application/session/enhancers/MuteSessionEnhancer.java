9d22d6301d2499ef79716cb646a38b05dc6d141d
DavidMaes <david.s.maes@gmail.com>
2022-08-02 00:01:11 +0200

    Change session enhancers to be async

--- src/main/java/net/shortninja/staffplus/core/application/session/enhancers/MuteSessionEnhancer.java
+++ src/main/java/net/shortninja/staffplus/core/application/session/enhancers/MuteSessionEnhancer.java
@@ -4,19 +4,22 @@ import be.garagepoort.mcioc.IocBean;
 import be.garagepoort.mcioc.IocMultiProvider;
 import net.shortninja.staffplus.core.application.session.PlayerSession;
 import net.shortninja.staffplus.core.application.session.SessionEnhancer;
+import net.shortninja.staffplus.core.common.utils.BukkitUtils;
 import net.shortninja.staffplus.core.domain.staff.mute.MuteService;
 
 @IocBean(conditionalOnProperty = "mute-module.enabled=true")
 @IocMultiProvider(SessionEnhancer.class)
 public class MuteSessionEnhancer implements SessionEnhancer {
     private final MuteService muteService;
+    private final BukkitUtils bukkitUtils;
 
-    public MuteSessionEnhancer(MuteService muteService) {
+    public MuteSessionEnhancer(MuteService muteService, BukkitUtils bukkitUtils) {
         this.muteService = muteService;
+        this.bukkitUtils = bukkitUtils;
     }
 
     @Override
     public void enhance(PlayerSession playerSession) {
-        muteService.getMuteByMutedUuid(playerSession.getUuid()).ifPresent((m) -> playerSession.setMuted(true));
+        bukkitUtils.runTaskAsync(() -> muteService.getMuteByMutedUuid(playerSession.getUuid()).ifPresent((m) -> playerSession.setMuted(true)));
     }
 }
