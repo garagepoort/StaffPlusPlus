3573532ab227faed6393fd418597b22b0986439b
DavidMaes <david.s.maes@gmail.com>
2022-12-23 20:40:12 +0100

    Add nightvision command. Add custom staff mode states

--- src/main/java/net/shortninja/staffplus/core/common/cmd/PlayerRetrievalStrategy.java
+++ src/main/java/net/shortninja/staffplus/core/common/cmd/PlayerRetrievalStrategy.java
@@ -1,5 +1,19 @@
 package net.shortninja.staffplus.core.common.cmd;
 
+/**
+ * Determines how the player should be retrieved when executing a command.
+ * Options:
+ *         - ONLINE
+ *           If specified the command will try to find an online player. If none found the command fails with error
+ *         - BOTH
+ *           If specified the command will try to find an online or offline player. If none found the command fails with error
+ *         - NONE
+ *           The command will never try to find a target player
+ *         - OPTIONAL_ONLINE
+ *           If specified the command will try to find an online player. If none found the command will not fail
+ *         - OPTIONAL_BOTH
+ *           If specified the command will try to find an online or offline player.  If none found the command will not fail
+ */
 public enum PlayerRetrievalStrategy {
     ONLINE,
     BOTH,
