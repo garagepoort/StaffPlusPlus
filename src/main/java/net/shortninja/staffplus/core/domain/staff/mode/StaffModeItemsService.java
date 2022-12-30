9b7aa0e4d99b7a80d435e1f9a22b23a452808945
DavidMaes <david.s.maes@gmail.com>
2022-12-31 00:46:41 +0100

    Fix state changes resetting inventory item locations

--- src/main/java/net/shortninja/staffplus/core/domain/staff/mode/StaffModeItemsService.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/mode/StaffModeItemsService.java
@@ -14,12 +14,15 @@ import net.shortninja.staffplus.core.domain.staff.mode.config.ModeItemConfigurat
 import net.shortninja.staffplus.core.domain.staff.mode.config.modeitems.vanish.VanishModeConfiguration;
 import net.shortninja.staffplus.core.domain.staff.mode.custommodules.CustomModuleConfiguration;
 import net.shortninja.staffplus.core.domain.staff.mode.custommodules.state.CustomModuleStateMachine;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -35,29 +38,30 @@ public class StaffModeItemsService {
     private final List<CustomModuleConfiguration> customModuleConfigurations;
     private final CustomModuleStateMachine customModuleStateMachine;
     private final IProtocolService protocolService;
-
+    private final ModeProvider modeProvider;
 
     public StaffModeItemsService(Options options, PlayerSettingsRepository playerSettingsRepository,
                                  CustomModuleStateMachine customModuleStateMachine,
                                  IProtocolService protocolService,
                                  @ConfigProperty("staffmode-custom-modules:custom-modules")
                                  @ConfigObjectList(CustomModuleConfiguration.class)
-                                 List<CustomModuleConfiguration> customModuleConfigurations) {
+                                 List<CustomModuleConfiguration> customModuleConfigurations, ModeProvider modeProvider) {
         this.playerSettingsRepository = playerSettingsRepository;
         this.customModuleConfigurations = customModuleConfigurations;
+        this.modeProvider = modeProvider;
 
         MODE_ITEMS = Stream.of(Arrays.asList(
-                options.staffItemsConfiguration.getCompassModeConfiguration(),
-                options.staffItemsConfiguration.getRandomTeleportModeConfiguration(),
-                options.staffItemsConfiguration.getVanishModeConfiguration(),
-                options.staffItemsConfiguration.getGuiModeConfiguration(),
-                options.staffItemsConfiguration.getCounterModeConfiguration(),
-                options.staffItemsConfiguration.getFreezeModeConfiguration(),
-                options.staffItemsConfiguration.getCpsModeConfiguration(),
-                options.staffItemsConfiguration.getExamineModeConfiguration(),
-                options.staffItemsConfiguration.getFollowModeConfiguration(),
-                options.staffItemsConfiguration.getPlayerDetailsModeConfiguration()
-            ),
+                    options.staffItemsConfiguration.getCompassModeConfiguration(),
+                    options.staffItemsConfiguration.getRandomTeleportModeConfiguration(),
+                    options.staffItemsConfiguration.getVanishModeConfiguration(),
+                    options.staffItemsConfiguration.getGuiModeConfiguration(),
+                    options.staffItemsConfiguration.getCounterModeConfiguration(),
+                    options.staffItemsConfiguration.getFreezeModeConfiguration(),
+                    options.staffItemsConfiguration.getCpsModeConfiguration(),
+                    options.staffItemsConfiguration.getExamineModeConfiguration(),
+                    options.staffItemsConfiguration.getFollowModeConfiguration(),
+                    options.staffItemsConfiguration.getPlayerDetailsModeConfiguration()
+                ),
                 customModuleConfigurations)
             .flatMap(Collection::stream)
             .collect(Collectors.toList());
@@ -66,28 +70,53 @@ public class StaffModeItemsService {
         this.protocolService = protocolService;
     }
 
+    public void setModuleItem(Player player, StaffModule staffModule, int slot) {
+        PlayerSettings playerSettings = playerSettingsRepository.get(player);
+        GeneralModeConfiguration modeConfiguration = modeProvider.getMode(player, playerSettings.getModeName().get());
+        setItem(player, modeConfiguration, playerSettings, staffModule.getKey(), slot);
+    }
+
     public void setStaffModeItems(Player player, GeneralModeConfiguration modeConfiguration) {
         PlayerSettings session = playerSettingsRepository.get(player);
         JavaUtils.clearInventory(player);
 
+        modeConfiguration.getItemSlots()
+            .forEach((moduleName, slot) -> setItem(player, modeConfiguration, session, moduleName, slot));
+    }
+
+    /**
+     * This methods assumes you are already in staff mode.
+     * It will refresh the entire inventory based on the mode configuration and current mode states
+     * Useful after a state change has occurred.
+     * @param player
+     * @param modeConfiguration
+     */
+    public void refreshStaffModeItems(Player player, GeneralModeConfiguration modeConfiguration) {
+        PlayerSettings session = playerSettingsRepository.get(player);
+        Map<String, Integer> currentLocationsOfModules = getCurrentLocationsOfModules(player);
+        JavaUtils.clearInventory(player);
+
         modeConfiguration.getItemSlots().forEach((moduleName, slot) -> {
-            Optional<? extends ModeItemConfiguration> module = getModule(moduleName);
-            if (!module.isPresent()) {
-                logger.warning("No module found with name [" + moduleName + "]. Skipping...");
-            } else {
-                setModeItem(player, session, module.get(), slot, modeConfiguration);
-            }
+            int locationOfItem = getNewLocationOfModuleItem(moduleName, modeConfiguration, currentLocationsOfModules);
+            setItem(player, modeConfiguration, session, moduleName, locationOfItem);
         });
     }
 
-    public void setModeItem(Player player, PlayerSettings session, ModeItemConfiguration modeItem, int slot, GeneralModeConfiguration modeConfiguration) {
+    public void setItem(Player player, GeneralModeConfiguration modeConfiguration, PlayerSettings session, String moduleName, int locationOfItem) {
+        Optional<? extends ModeItemConfiguration> module = getModule(moduleName);
+        if (!module.isPresent()) {
+            logger.warning("No module found with name [" + moduleName + "]. Skipping...");
+            return;
+        }
+
+        ModeItemConfiguration modeItem = module.get();
         if (!modeItem.isEnabled()) {
             return;
         }
 
-        if(modeItem instanceof CustomModuleConfiguration) {
+        if (modeItem instanceof CustomModuleConfiguration) {
             CustomModuleConfiguration customModuleConfiguration = (CustomModuleConfiguration) modeItem;
-            if(customModuleConfiguration.getEnabledOnState().isPresent() && !customModuleStateMachine.isActive(player, customModuleConfiguration.getEnabledOnState().get())) {
+            if (customModuleConfiguration.getEnabledOnState().isPresent() && !customModuleStateMachine.isActive(player, customModuleConfiguration.getEnabledOnState().get())) {
                 return;
             }
         }
@@ -95,10 +124,10 @@ public class StaffModeItemsService {
         if (modeItem instanceof VanishModeConfiguration) {
             ItemStack modeVanishItem = ((VanishModeConfiguration) modeItem).getModeVanishItem(session, modeConfiguration.getModeVanish());
             ItemStack item = protocolService.getVersionProtocol().addNbtString(modeVanishItem, modeItem.getIdentifier());
-            player.getInventory().setItem(slot, item);
+            player.getInventory().setItem(locationOfItem, item);
         } else {
             ItemStack item = protocolService.getVersionProtocol().addNbtString(modeItem.getItem(), modeItem.getIdentifier());
-            player.getInventory().setItem(slot, item);
+            player.getInventory().setItem(locationOfItem, item);
         }
     }
 
@@ -122,5 +151,34 @@ public class StaffModeItemsService {
     public Optional<CustomModuleConfiguration> getCustomModule(ItemStack item) {
         String identifier = protocolService.getVersionProtocol().getNbtString(item);
         return getCustomModule(identifier);
+    }
+
+    private int getNewLocationOfModuleItem(String moduleToSet, GeneralModeConfiguration modeConfiguration, Map<String, Integer> currentLocationsOfModules) {
+        if (currentLocationsOfModules.containsKey(moduleToSet)) {
+            return currentLocationsOfModules.get(moduleToSet);
+        }
+
+        List<String> modulesWithSameLocation = modeConfiguration.getModulesWithSameLocation(moduleToSet);
+        for (String module : modulesWithSameLocation) {
+            if (currentLocationsOfModules.containsKey(module)) {
+                return currentLocationsOfModules.get(module);
+            }
+        }
+        return modeConfiguration.getItemSlots().get(moduleToSet);
+    }
+
+    private Map<String, Integer> getCurrentLocationsOfModules(Player player) {
+        Map<String, Integer> result = new HashMap<>();
+        ItemStack[] contents = player.getInventory().getContents();
+        for (int i = 0; i < contents.length; i++) {
+            ItemStack item = contents[i];
+            if (item != null) {
+                String moduleName = protocolService.getVersionProtocol().getNbtString(item);
+                if (StringUtils.isNotEmpty(moduleName)) {
+                    result.put(moduleName, i);
+                }
+            }
+        }
+        return result;
     }
 }
