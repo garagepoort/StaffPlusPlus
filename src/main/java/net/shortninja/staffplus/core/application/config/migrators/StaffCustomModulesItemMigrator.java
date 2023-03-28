04d4ed0917f400ad9aa141400bd5d1c5d0a2d804
DavidMaes <david.s.maes@gmail.com>
2023-03-28 23:11:03 +0200

    Fix custom-modules migrator

--- src/main/java/net/shortninja/staffplus/core/application/config/migrators/StaffCustomModulesItemMigrator.java
+++ src/main/java/net/shortninja/staffplus/core/application/config/migrators/StaffCustomModulesItemMigrator.java
@@ -1,11 +1,11 @@
 package net.shortninja.staffplus.core.application.config.migrators;
 
 import be.garagepoort.mcioc.configuration.files.ConfigurationFile;
+import be.garagepoort.mcioc.configuration.yaml.configuration.ConfigurationSection;
 import be.garagepoort.mcioc.configuration.yaml.configuration.file.FileConfiguration;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 public class StaffCustomModulesItemMigrator implements StaffPlusPlusConfigMigrator {
@@ -17,17 +17,26 @@ public class StaffCustomModulesItemMigrator implements StaffPlusPlusConfigMigrat
 
     private void migrateModules(List<ConfigurationFile> configs, String identifier, String path) {
         FileConfiguration customModulesConfig = getConfig(configs, identifier);
-        List<LinkedHashMap<String, Object>> modules = (List<LinkedHashMap<String, Object>>) customModulesConfig.getList(path, new ArrayList<>());
-        modules.forEach(map -> {
-            if (map.get("item") instanceof String) {
-                HashMap<String, String> value = new HashMap<>();
-                value.put("type", (String) map.get("item"));
-                value.put("name", (String) map.get("name"));
-                value.put("lore", (String) map.get("lore"));
-                map.put("item", value);
-                map.remove("lore");
+
+        boolean configurationSection = customModulesConfig.isConfigurationSection("custom-modules");
+        if(configurationSection) {
+            ConfigurationSection modules = customModulesConfig.getConfigurationSection("custom-modules");
+            List<ConfigurationSection> newModules = new ArrayList<>();
+            if (modules != null) {
+                modules.getKeys(false).forEach(k -> {
+                    ConfigurationSection module = modules.getConfigurationSection(k);
+                    if(module.get("item") instanceof String) {
+                        HashMap<String, String> value = new HashMap<>();
+                        value.put("type", (String) module.get("item"));
+                        value.put("name", (String) module.get("name"));
+                        value.put("lore", (String) module.get("lore"));
+                        module.set("item", value);
+                        module.set("lore", null);
+                        newModules.add(module);
+                    }
+                });
+                customModulesConfig.set("custom-modules", newModules);
             }
-        });
-        customModulesConfig.set(path, modules);
+        }
     }
 }
