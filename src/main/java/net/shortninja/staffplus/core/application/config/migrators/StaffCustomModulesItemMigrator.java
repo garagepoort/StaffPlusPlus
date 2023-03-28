c81a00a9de27953aec0b4d206e2afd75740c58ce
Cardinal System <cardinalsystemofficial@gmail.com>
2023-03-27 21:39:20 -0400

    Fixed ClassCastException

--- src/main/java/net/shortninja/staffplus/core/application/config/migrators/StaffCustomModulesItemMigrator.java
+++ src/main/java/net/shortninja/staffplus/core/application/config/migrators/StaffCustomModulesItemMigrator.java
@@ -1,11 +1,11 @@
 package net.shortninja.staffplus.core.application.config.migrators;
 
 import be.garagepoort.mcioc.configuration.files.ConfigurationFile;
+import be.garagepoort.mcioc.configuration.yaml.configuration.MemorySection;
 import be.garagepoort.mcioc.configuration.yaml.configuration.file.FileConfiguration;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 public class StaffCustomModulesItemMigrator implements StaffPlusPlusConfigMigrator {
@@ -17,17 +17,17 @@ public class StaffCustomModulesItemMigrator implements StaffPlusPlusConfigMigrat
 
     private void migrateModules(List<ConfigurationFile> configs, String identifier, String path) {
         FileConfiguration customModulesConfig = getConfig(configs, identifier);
-        List<LinkedHashMap<String, Object>> modules = (List<LinkedHashMap<String, Object>>) customModulesConfig.getList(path, new ArrayList<>());
-        modules.forEach(map -> {
-            if (map.get("item") instanceof String) {
-                HashMap<String, String> value = new HashMap<>();
-                value.put("type", (String) map.get("item"));
-                value.put("name", (String) map.get("name"));
-                value.put("lore", (String) map.get("lore"));
-                map.put("item", value);
-                map.remove("lore");
-            }
-        });
-        customModulesConfig.set(path, modules);
+		List<MemorySection> modules = (List<MemorySection>) customModulesConfig.getList(path, new ArrayList<>());
+		modules.forEach(section -> {
+			if (section.get("item") instanceof String) {
+				HashMap<String, String> value = new HashMap<>();
+				value.put("type", section.getString("item"));
+				value.put("name", section.getString("name"));
+				value.put("lore", section.getString("lore"));
+				section.set("item", value);
+				section.set("lore", null);
+			}
+		});
+		customModulesConfig.set(path, modules);
     }
 }
