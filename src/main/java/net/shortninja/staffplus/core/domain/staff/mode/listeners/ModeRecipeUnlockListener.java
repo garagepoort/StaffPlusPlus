a19890f49c6b481ddbc0a8777f5faca56f018621
Adamiok <adam@kresta.eu>
2024-06-23 12:08:15 +0400

    Do not grant recipes in mode

---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/mode/listeners/ModeRecipeUnlockListener.java
@@ -0,0 +1,29 @@
+package net.shortninja.staffplus.core.domain.staff.mode;
+
+import be.garagepoort.mcioc.tubingbukkit.annotations.IocBukkitListener;
+import net.shortninja.staffplus.core.application.session.OnlinePlayerSession;
+import net.shortninja.staffplus.core.application.session.OnlineSessionsManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRecipeDiscoverEvent;
+
+@IocBukkitListener
+public class ModeRecipeUnlockListener implements Listener {
+    private final OnlineSessionsManager sessionManager;
+
+    public ModeRecipeUnlockListener(OnlineSessionsManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onRecipeUnlock(PlayerRecipeDiscoverEvent event)  {
+        Player player = event.getPlayer();
+        OnlinePlayerSession session = sessionManager.get(player);
+        
+        if (!session.isInStaffMode()) return;
+        
+        event.setCancelled(true);
+    }
+}
