941a842f9bdb0f6741f6fc62db9f8799ac5f9e65
david <david@ptr-917cxxs3wlhoe5xryyt.18120a2.ip6.access.telenet.be>
2024-11-01 20:14:31 +0100

    Add gson serializers for java time

--- src/main/java/net/shortninja/staffplus/core/common/bungee/GsonParser.java
+++ src/main/java/net/shortninja/staffplus/core/common/bungee/GsonParser.java
@@ -1,8 +1,13 @@
 package net.shortninja.staffplus.core.common.bungee;
 
 import be.garagepoort.mcioc.IocBean;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @IocBean
 public class GsonParser {
@@ -10,7 +15,16 @@ public class GsonParser {
     private final Gson gson;
 
     public GsonParser() {
-        gson = new GsonBuilder().registerTypeAdapterFactory(OptionalTypeAdapter.FACTORY).create();
+        gson = new GsonBuilder().registerTypeAdapterFactory(OptionalTypeAdapter.FACTORY)
+            .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer<LocalDateTime>) (json, type, jsonDeserializationContext) -> {
+                Instant instant = Instant.ofEpochMilli(json.getAsJsonPrimitive().getAsLong());
+                return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+            })
+            .registerTypeAdapter(ZonedDateTime.class, (JsonDeserializer<ZonedDateTime>) (json, type, jsonDeserializationContext) -> {
+                Instant instant = Instant.ofEpochMilli(json.getAsJsonPrimitive().getAsLong());
+                return ZonedDateTime.ofInstant(instant, ZoneId.systemDefault());
+            })
+            .create();
     }
 
     public String toJson(Object src) {
