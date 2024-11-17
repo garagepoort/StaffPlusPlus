2f87c3fdd9febb1a105bf9deaead991227125ca2
David Maes <david.s.maes@gmail.com>
2024-11-17 11:23:35 +0100

    Merge pull request #252 from Adamiok/improvements
    Improvements & Bug Fixes

--- src/main/java/net/shortninja/staffplus/core/domain/staff/mode/listeners/ModeAdvancementListener.java
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/mode/listeners/ModeAdvancementListener.java
@@ -16,6 +16,9 @@ import org.bukkit.scheduler.BukkitRunnable;
 @IocBukkitListener
 public class ModeAdvancementListener implements Listener {
     private final OnlineSessionsManager sessionManager;
+    
+    // Used to prevent race conditions if multiple achievements are awarded
+    private boolean announceGameruleDisabledByUs = false;
 
     public ModeAdvancementListener(OnlineSessionsManager sessionManager) {
         this.sessionManager = sessionManager;
@@ -27,10 +30,12 @@ public class ModeAdvancementListener implements Listener {
         OnlinePlayerSession session = sessionManager.get(player);
         
         if (!session.isInStaffMode()) return;
+        if (!(player.getWorld().getGameRuleValue(GameRule.ANNOUNCE_ADVANCEMENTS) || announceGameruleDisabledByUs)) return;
         
         Advancement advancement = event.getAdvancement();
         
         // There is no better way to do it than to disable the gamerule and then enable it
+        announceGameruleDisabledByUs = true;
         player.getWorld().setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false);
         
         for (String criteria : advancement.getCriteria()) {
@@ -42,6 +47,7 @@ public class ModeAdvancementListener implements Listener {
             @Override
             public void run() {
                 player.getWorld().setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, true);
+                announceGameruleDisabledByUs = false;
             }
         }.runTask(StaffPlusPlus.get());
     }
