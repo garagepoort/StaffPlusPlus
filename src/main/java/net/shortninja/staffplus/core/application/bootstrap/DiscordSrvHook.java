dc5601a7dec98497e77540406066588a2d7df4be
Adamiok <adam@kresta.eu>
2025-05-03 16:25:13 +0400

    Add join/leave messages

--- src/main/java/net/shortninja/staffplus/core/application/bootstrap/DiscordSrvHook.java
+++ src/main/java/net/shortninja/staffplus/core/application/bootstrap/DiscordSrvHook.java
@@ -5,20 +5,29 @@ import github.scarsz.discordsrv.DiscordSRV;
 import net.shortninja.staffplus.core.StaffPlusPlus;
 import net.shortninja.staffplus.core.application.session.OnlineSessionsManager;
 import net.shortninja.staffplus.core.domain.staff.mode.StaffModeDiscordSrvListener;
+import net.shortninja.staffplus.core.domain.staff.vanish.listeners.VanishJoinLeaveDiscordSrvMessageListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 @IocBean
 public class DiscordSrvHook {
     
-    private StaffModeDiscordSrvListener staffListener = null;
+    private StaffModeDiscordSrvListener staffListener;
+    private VanishJoinLeaveDiscordSrvMessageListener vanishMessageListener;
     
     public DiscordSrvHook(OnlineSessionsManager sessionManager) {
         Plugin discordSrvPlugin = Bukkit.getPluginManager().getPlugin("DiscordSRV");
         
-        if (discordSrvPlugin != null && discordSrvPlugin.isEnabled()) {
-            staffListener = new StaffModeDiscordSrvListener(sessionManager);
-            DiscordSRV.api.subscribe(staffListener);
+        if (!(discordSrvPlugin != null && discordSrvPlugin.isEnabled())) {
+            return;
         }
+        
+        staffListener = new StaffModeDiscordSrvListener(sessionManager);
+        vanishMessageListener = new VanishJoinLeaveDiscordSrvMessageListener();
+        
+        DiscordSRV.api.subscribe(staffListener);
+        
+        
+        StaffPlusPlus.get().getLogger().info("Hooked into DiscordSRV " + discordSrvPlugin.getDescription().getVersion());
     }
 }
---
+++ src/main/java/net/shortninja/staffplus/core/domain/staff/vanish/listeners/VanishJoinLeaveDiscordSrvMessageListener.java
@@ -0,0 +1,42 @@
+package net.shortninja.staffplus.core.domain.staff.vanish.listeners;
+
+import be.garagepoort.mcioc.configuration.ConfigProperty;
+import github.scarsz.discordsrv.DiscordSRV;
+import net.shortninja.staffplus.core.StaffPlusPlus;
+import net.shortninja.staffplusplus.vanish.VanishOffEvent;
+import net.shortninja.staffplusplus.vanish.VanishOnEvent;
+import net.shortninja.staffplusplus.vanish.VanishType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class VanishJoinLeaveDiscordSrvMessageListener implements Listener {
+    
+    @ConfigProperty("vanish-module.join-leave-message-enabled")
+    private boolean vanishMessagesEnabled;
+    @ConfigProperty("%lang%:vanish-join-message")
+    private String vanishJoinMessage;
+    @ConfigProperty("%lang%:vanish-leave-message")
+    private String vanishLeaveMessage;
+    
+    public VanishJoinLeaveDiscordSrvMessageListener() {
+        StaffPlusPlus.get().getServer().getPluginManager().registerEvents(this, StaffPlusPlus.get());
+    }
+    
+    @EventHandler
+    public void onVanish(VanishOnEvent event) {
+        if (!vanishMessagesEnabled || event.isOnJoin() || !(event.getType() == VanishType.LIST || event.getType() == VanishType.TOTAL)) {
+            return;
+        }
+        
+        DiscordSRV.getPlugin().sendLeaveMessage(event.getPlayer(), vanishLeaveMessage.replace("%player%", event.getPlayer().getName()));
+    }
+
+    @EventHandler
+    public void onUnvanish(VanishOffEvent event) {
+        if (!vanishMessagesEnabled || !(event.getType() == VanishType.LIST || event.getType() == VanishType.TOTAL)) {
+            return;
+        }
+        
+        DiscordSRV.getPlugin().sendJoinMessage(event.getPlayer(), vanishJoinMessage.replace("%player%", event.getPlayer().getName()));
+    }
+}
